43f9b2e9eeaaf4c50669c97ac8254b7a33f6528d
Jack Gerrits <jackgerrits95@gmail.com>
2018-10-22 12:17:51 -0400

    Update package paths, update build script to check for VcpkgInstallRoot, update caching

--- build-windows.cmd
+++ build-windows.cmd
@@ -1,12 +1,18 @@
 @echo off
+setlocal
 
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
   set InstallDir=%%i
 )
 
 if not exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
-   echo "MsBuild couldn't be found"
-   exit 1
+  echo ERROR: MsBuild couldn't be found
+  exit /b 1
+)
+
+if not defined VcpkgInstallRoot (
+  echo ERROR: VcpkgInstallRoot must be set
+  exit /b 1
 )
 
 call "%InstallDir%\Common7\Tools\VsDevCmd.bat"
