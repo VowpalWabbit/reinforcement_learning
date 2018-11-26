c3b9e2b4311e44da1f5fbdc6b2791eec4b017d38
Rajan Chari <ranaras@microsoft.com>
2018-11-26 14:31:42 -0500

    fix formatting issues

--- include/sender.h
+++ include/sender.h
@@ -8,13 +8,14 @@ namespace reinforcement_learning {
   class i_sender {
   public:
     using buffer = std::shared_ptr<utility::data_buffer>;
-    virtual int init(api_status* status) = 0;
-    int send(buffer& data, api_status* status = nullptr) {
+    virtual int init(api_status *status) = 0;
+    int send(buffer &data, api_status *status = nullptr) {
       return v_send(data, status);
     }
 
     virtual ~i_sender() = default;
+
   protected:
-    virtual int v_send(buffer& data, api_status* status = nullptr) = 0;
+    virtual int v_send(buffer &data, api_status *status = nullptr) = 0;
   };
-}
+} // namespace reinforcement_learning
