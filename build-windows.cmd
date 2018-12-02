eb9b4cc5067603224ea7dc1953d601ac6d7f2dd7
Rajan Chari <rajan.chari@yahoo.com>
2018-12-02 23:33:36 +0000

    trigger appveyor cache refresh

--- build-windows.cmd
+++ build-windows.cmd
@@ -30,6 +30,7 @@ nuget install -o packages bindings\cs\rl.net.native\packages.config
 nuget install -o packages rlclientlib\packages.config
 nuget install -o packages unit_tests\packages.config
 
+
 dotnet restore rl.sln
 
 "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" "rl.sln" /m /verbosity:normal /p:Configuration=Debug;Platform=x64
