a1475e421c0de5a8bc07b749b317b2eda297cd01
Jacob Alber <jalber@fernir.com>
2019-03-01 12:30:24 -0500

    Break out build-windows.cmd into per-stage scripts

---
+++ .scripts/init.cmd
@@ -0,0 +1,49 @@
+REM Integration points for toolchain customization
+IF NOT DEFINED nugetPath (
+    SET nugetPath=nuget
+)
+
+IF NOT DEFINED msbuildPath (
+    REM Try to find VS Install
+    for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+        set InstallDir=%%i
+    )
+
+    if not exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
+        echo ERROR: MsBuild couldn't be found
+        exit /b 1
+    )
+    else (
+        SET "msBuildPath=%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+    )
+)
+
+IF NOT DEFINED dotnetPath (
+    SET dotnetPath=dotnet
+)
+
+IF NOT DEFINED vcpkgPath (
+    IF NOT DEFINED VcpkgInstallRoot (
+        ECHO ERROR: vcpkgPath is not configured, but VcpkgInstallRoot is also not configured. Cannot find vcpkg.
+        EXIT /b 1
+    )
+
+    SET "vcpkgPath=%VcpkgInstallRoot%vcpkg.exe"
+    SET "VcpkgIntegration=%VcpkgInstallRoot%scripts\buildsystems\msbuild\vcpkg.targets"
+)
+
+REM The correct way to integrate this is to use msbuild targets and contribute them back to flatbuff, 
+REM rather than a command line execution
+IF NOT DEFINED flatcPath (
+    IF NOT DEFINED VcpkgInstallRoot (
+        ECHO ERROR: flatcPath is not configured, but VcpkgInstallRoot is also not configured. Cannot find vcpkg.
+        EXIT /b 1
+    )
+
+    SET "flatcPath=%VcpkgInstallRoot%installed\x64-windows\tools\flatbuffers\flatc.exe"
+)
+
+REM Repo-specific paths
+IF NOT DEFINED rlRoot (
+    SET rlRoot=%~dp0..
+)
---
+++ .scripts/restore.cmd
@@ -0,0 +1,41 @@
+@ECHO OFF
+IF DEFINED DebugBuildScripts (
+    @ECHO ON
+)
+
+SETLOCAL
+
+CALL %~dp0init.cmd
+PUSHD %~dp0
+
+%vcpkgPath% install cpprestsdk:x64-windows
+%vcpkgPath% install flatbuffers:x64-windows
+
+REM TODO: This really should be out-of-source (also, can we switch to vcpkg for these?)
+ECHO Restoring "%rlRoot%\ext_libs\vowpal_wabbit\vowpalwabbit\packages.config"
+"%nugetPath%" install "%rlRoot%\ext_libs\vowpal_wabbit\vowpalwabbit\packages.config" -solutionDir "%rlRoot%\"
+ECHO.
+
+ECHO Restoring "%rlRoot%\bindings\cs\rl.net.native\packages.config"
+"%nugetPath%" install -o packages "%rlRoot%\bindings\cs\rl.net.native\packages.config"
+ECHO.
+
+ECHO Restoring "%rlRoot%\ext_libs\vowpal_wabbit\vowpalwabbit\packages.config"
+"%nugetPath%" install -o packages "%rlRoot%\rlclientlib\packages.config"
+ECHO.
+
+ECHO Restoring "%rlRoot%\ext_libs\vowpal_wabbit\vowpalwabbit\packages.config"
+"%nugetPath%" install -o packages "%rlRoot%\unit_tests\packages.config"
+ECHO.
+
+ECHO Restoring SDK-Style projects in "%rlRoot%\rl.sln"
+%dotnetPath% restore "%rlRoot%\rl.sln"
+ECHO.
+
+REM TODO: Is this still necessary?
+SET PATH=%PATH%;%VcpkgInstallRoot%installed\x64-windows\tools\flatbuffers
+
+
+POPD
+
+ENDLOCAL
