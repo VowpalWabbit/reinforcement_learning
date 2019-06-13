d3cfc792816e90168590798462e524668aaac741
rajanjedi <rajanjedi@users.noreply.github.com>
2019-06-13 09:05:26 -0400

    Rajan/model file system loader (#73)
    * Added file system loader for models
    
    * Added filesystem loader to CMake

--- rlclientlib/factory_resolver.cc
+++ rlclientlib/factory_resolver.cc
@@ -14,7 +14,7 @@
 #include "console_tracer.h"
 #include "error_callback_fn.h"
 #include "logger/file/file_logger.h"
-
+#include "model_mgmt/file_model_loader.h"
 namespace reinforcement_learning {
   namespace m = model_management;
   namespace u = utility;
@@ -87,6 +87,24 @@ namespace reinforcement_learning {
     return error_code::success;
   }
 
+  int file_model_loader_create(m::i_data_transport** retval, const u::configuration& config, i_trace* trace_logger, api_status* status)
+  {
+    TRACE_INFO(trace_logger, "File model loader created.");
+    const char* file_name = config.get(name::MODEL_FILE_NAME, "current");
+    const bool file_must_exist = config.get_bool(name::MODEL_FILE_MUST_EXIST, false);
+    auto file_loader = new model_management::file_model_loader(file_name, file_must_exist, trace_logger);
+    
+    const auto success = file_loader->init(status);
+
+    if (success != error_code::success) {
+      delete file_loader;
+      return success;
+    }
+
+    *retval = file_loader;
+    return error_code::success;
+  }
+  
   int null_time_provider_create(i_time_provider** retval, const u::configuration& config, i_trace* trace_logger, api_status* status)
   {
     TRACE_INFO(trace_logger, "Null time provider created.");
@@ -103,11 +121,16 @@ namespace reinforcement_learning {
 
   void factory_initializer::register_default_factories() {
     register_azure_factories();
+    
     data_transport_factory.register_type(value::NO_MODEL_DATA, empty_data_transport_create);
+    data_transport_factory.register_type(value::FILE_MODEL_DATA, file_model_loader_create);
+
     model_factory.register_type(value::VW, model_create<m::vw_model>);
     model_factory.register_type(value::PASSTHROUGH_PDF_MODEL, model_create<m::pdf_model>);
+    
     trace_logger_factory.register_type(value::NULL_TRACE_LOGGER, null_tracer_create);
     trace_logger_factory.register_type(value::CONSOLE_TRACE_LOGGER, console_tracer_create);
+    
     time_provider_factory.register_type(value::NULL_TIME_PROVIDER, null_time_provider_create);
     time_provider_factory.register_type(value::CLOCK_TIME_PROVIDER, clock_time_provider_create);
 
---
+++ rlclientlib/model_mgmt/file_model_loader.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "model_mgmt.h"
+namespace reinforcement_learning {
+  class i_trace;
+}
+
+namespace reinforcement_learning { namespace model_management {
+
+  class file_model_loader : public i_data_transport {
+  public:
+    file_model_loader(std::string filename, bool file_must_exist, i_trace* trace_logger);
+    int init(api_status* status = nullptr);
+    int get_data(model_data& data, api_status* status = nullptr) override;
+
+    private:
+    int get_file_modified_time(time_t& file_time, api_status* status) const;
+
+  private:
+    std::string _file_name;
+    bool _file_must_exist;
+    i_trace* _trace;
+    time_t _last_modified = 0;
+    size_t _datasz;
+  };
+
+}}
