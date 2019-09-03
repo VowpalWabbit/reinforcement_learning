651fe8d625db88e22cf6487c71c724ae1dd70c25
Jack Gerrits <jackgerrits95@gmail.com>
2019-09-03 18:34:57 -0400

    relax test

--- .scripts/linux/test.sh
+++ .scripts/linux/test.sh
@@ -9,4 +9,6 @@ REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR/build
 
 # Run unit test suite
-./unit_test/rltest --log_format=JUNIT --log_sink=TEST-rlclientlib.xml
+# ./unit_test/rltest --log_format=JUNIT --log_sink=TEST-rlclientlib.xml
+export CTEST_OUTPUT_ON_FAILURE=1
+make test
