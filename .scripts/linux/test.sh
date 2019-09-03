57a901df674b23470005ecea7436772c02af848f
Jack Gerrits <jackgerrits95@gmail.com>
2019-09-03 17:50:32 -0400

    report test status

--- .scripts/linux/test.sh
+++ .scripts/linux/test.sh
@@ -9,5 +9,4 @@ REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR/build
 
 # Run unit test suite
-export CTEST_OUTPUT_ON_FAILURE=1
-make test
+./unit_test/rltest --log_format=JUNIT --log_sink=TEST-rlclientlib.xml
