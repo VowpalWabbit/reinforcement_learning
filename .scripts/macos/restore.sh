175ee4845aa9eee69c71aa9eee5090b329df60a8
Jack Gerrits <jackgerrits95@gmail.com>
2019-09-03 17:35:02 -0400

    not able to specify openssl version

--- .scripts/macos/restore.sh
+++ .scripts/macos/restore.sh
@@ -3,5 +3,4 @@
 set -e
 set -x
 
-# Install specifc version of OpenSSL as we need to refer to the install directory specifically
-brew install cmake boost cpprestsdk flatbuffers openssl@1.0.2r
+brew install cmake boost cpprestsdk flatbuffers openssl
