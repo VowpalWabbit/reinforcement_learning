4e619fd4e70ba85bfa4c4643868f219146ef1274
Chenxi Zhao <homezcx616@gmail.com>
2020-02-25 17:24:33 -0800

    restore test files

--- unit_test/fb_serializer_test.cc
+++ unit_test/fb_serializer_test.cc
@@ -2,8 +2,6 @@
 #ifdef STAND_ALONE
 #   define BOOST_TEST_MODULE Main
 #endif
-#include <boost/test/included/unit_test.hpp>
-
 
 #include <boost/test/unit_test.hpp>
 #include "decision_modes.h"
--- unit_test/live_model_test.cc
+++ unit_test/live_model_test.cc
@@ -1,4 +1,7 @@
-#define BOOST_TEST_MODULE livemodeltest
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#   define BOOST_TEST_MODULE Main
+#endif
 
 #include <thread>
 #include <boost/test/unit_test.hpp>
