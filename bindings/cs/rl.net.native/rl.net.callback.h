bc45fc7ca9d23182ce55560b9b1da817661195bf
Alexey Taymanov <ataymano@microsoft.com>
2020-03-11 12:04:06 -0400

    typdefe -> using

--- bindings/cs/rl.net.native/rl.net.callback.h
+++ bindings/cs/rl.net.native/rl.net.callback.h
@@ -6,7 +6,8 @@ namespace rl_net_native {
   namespace constants {
     const char *const BINDING_TRACE_LOGGER = "BINDING_TRACE_LOGGER";
   }
-  typedef void(*background_error_callback_t)(const reinforcement_learning::api_status&);
-  typedef void(*trace_logger_callback_t)(int log_level, const char* msg);
+
+  using background_error_callback_t = void(*)(const reinforcement_learning::api_status&);
+  using trace_logger_callback_t = void(*)(int log_level, const char* msg);
 }
 
