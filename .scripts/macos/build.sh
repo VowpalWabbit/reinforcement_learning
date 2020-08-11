2760b1ea09523678547e11b745b346648793bc57
Jack Gerrits <jack@jackgerrits.com>
2020-08-11 09:03:09 -0400

    Use brew to find openssl dir

--- .scripts/macos/build.sh
+++ .scripts/macos/build.sh
@@ -10,7 +10,7 @@ mkdir -p build
 cd build
 
 # On MacOS CMake is unable to find brew installed OpenSSL, so we need to pass it where to look.
-OPEN_SSL_DIR="/usr/local/Cellar/openssl/1.0.2s"
+OPEN_SSL_DIR=`brew --prefix openssl`
 
 cmake .. -DOPENSSL_ROOT_DIR=$OPEN_SSL_DIR -DOPENSSL_LIBRARIES=$OPEN_SSL_DIR/lib
 NUM_PROCESSORS=$(cat nprocs.txt)
