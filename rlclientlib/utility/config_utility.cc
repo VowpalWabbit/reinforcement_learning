7291d4474320b0a850a28a7013d0be034bad967c
Schuyler Goodman <schuyler.goodman@gmail.com>
2020-09-22 02:28:13 -0700

    Remove unnecessary is_utf8 function added for debugging

--- rlclientlib/utility/config_utility.cc
+++ rlclientlib/utility/config_utility.cc
@@ -93,51 +93,6 @@ namespace reinforcement_learning { namespace utility { namespace config {
     return from;
   }
 
-  bool is_utf8(const char * string)
-  {
-      if (!string)
-          return true;
-
-      const unsigned char * bytes = (const unsigned char *)string;
-      int num;
-
-      while (*bytes != 0x00)
-      {
-          if ((*bytes & 0x80) == 0x00)
-          {
-              // U+0000 to U+007F 
-              num = 1;
-          }
-          else if ((*bytes & 0xE0) == 0xC0)
-          {
-              // U+0080 to U+07FF 
-              num = 2;
-          }
-          else if ((*bytes & 0xF0) == 0xE0)
-          {
-              // U+0800 to U+FFFF 
-              num = 3;
-          }
-          else if ((*bytes & 0xF8) == 0xF0)
-          {
-              // U+10000 to U+10FFFF 
-              num = 4;
-          }
-          else
-              return false;
-
-          bytes += 1;
-          for (int i = 1; i < num; ++i)
-          {
-              if ((*bytes & 0xC0) != 0x80)
-                  return false;
-              bytes += 1;
-          }
-      }
-
-      return true;
-  }
-
   int create_from_json(const std::string& config_json, configuration& cc, i_trace* trace, api_status* status) {
     static const std::map<std::string, std::string> legacy_translation_mapping = {
       { "ApplicationID"             , name::APP_ID },
