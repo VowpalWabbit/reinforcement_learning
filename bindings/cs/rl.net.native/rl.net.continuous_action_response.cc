4f22dc2c1b82d9e7e1a4b9a93a2a91eb6a7a9123
olgavrou <olvrousg@microsoft.com>
2020-10-01 22:03:21 +0100

    Continuous actions bindings (#195)
    * add continuous actions to model, v2 schema, simulator, basic usage and mock for testing
    
    * full simulation example
    
    * continuous actions disabled by flag
    
    * fix test
    
    * add overrides
    
    * cleanup
    
    * add files to vcxproj
    
    * add invalid ctx unit test
    
    * fix enums, remove flag, fix cmakelist
    
    * add event id to a cont actions constructor
    
    * add cs bindings and unit test
    
    * revert csproj change
    
    * fix typos
    
    * fix merge
    
    * renaming and add files to CMakeList
    
    Co-authored-by: Rodrigo Kumpera <kumpera@users.noreply.github.com>

---
+++ bindings/cs/rl.net.native/rl.net.continuous_action_response.cc
@@ -0,0 +1,31 @@
+#include "rl.net.continuous_action_response.h"
+
+API reinforcement_learning::continuous_action_response* CreateContinuousActionResponse()
+{
+    return new reinforcement_learning::continuous_action_response();
+}
+
+API void DeleteContinuousActionResponse(reinforcement_learning::continuous_action_response* response)
+{
+    delete response;
+}
+
+API const char* GetContinuousActionEventId(reinforcement_learning::continuous_action_response* response)
+{
+    return response->get_event_id();
+}
+
+API const char* GetContinuousActionModelId(reinforcement_learning::continuous_action_response* response)
+{
+    return response->get_model_id();
+}
+
+API float GetContinuousActionChosenAction(reinforcement_learning::continuous_action_response* response)
+{
+    return response->get_chosen_action();
+}
+
+API float GetContinuousActionChosenActionDensity(reinforcement_learning::continuous_action_response* response)
+{
+    return response->get_chosen_action_pdf_value();
+}
