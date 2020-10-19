7f2024586a7b08f11b5f795d13de88447fcebf74
Rodrigo Kumpera <kumpera@users.noreply.github.com>
2020-10-19 12:32:37 -0400

    Add dedup_dict class that will later be used by the dedup work. (#207)
    * Change get_context_info to return index+length instead of index+index.
    
    * Add initial bits of dedup dictionary.
    
    * Add test for the transformation.
    
    * Test string parsing.
    
    * add missing header.
    
    * Rename transform_payload and make get_action const.

---
+++ rlclientlib/dedup.cc
@@ -0,0 +1,83 @@
+#include "dedup.h"
+#include "hash.h"
+#include "utility/context_helper.h"
+
+#include <sstream>
+
+namespace reinforcement_learning
+{
+  namespace u = utility;
+
+  dedup_dict::dict_entry::dict_entry(const char *data, size_t length) : _count(1),
+                                                                        _length(length),
+                                                                        _content(data, data + length)
+  {
+  }
+
+  static dedup_dict::action_id_t hash_content(const char *start, size_t size)
+  {
+    return uniform_hash(start, size, 0);
+  }
+
+  dedup_dict::action_id_t dedup_dict::add_action(const char *start, size_t length)
+  {
+    action_id_t hash = hash_content(start, length);
+    auto it = _entries.find(hash);
+    if (it == _entries.end())
+    {
+      _entries.insert({ hash, dict_entry(start, length) });
+    }
+    else
+    {
+      ++it->second._count;
+    }
+    return hash;
+  }
+
+  bool dedup_dict::remove_action(action_id_t aid)
+  {
+    auto it = _entries.find(aid);
+    if (it == _entries.end())
+      return false;
+
+    --it->second._count;
+    if (!it->second._count)
+      _entries.erase(it);
+
+    return true;
+  }
+
+  string_view dedup_dict::get_action(action_id_t aid) const
+  {
+    auto it = _entries.find(aid);
+    if (it == _entries.end())
+      return string_view();
+    return string_view(it->second._content.data(), it->second._length);
+  }
+
+  int dedup_dict::transform_payload_and_add_actions(const char* payload, std::string& edited_payload, action_list_t& action_ids, api_status* status)
+  {
+    u::ContextInfo context_info;
+    RETURN_IF_FAIL(u::get_context_info(payload, context_info, nullptr, status));
+
+    edited_payload = payload;
+    action_ids.clear();
+    action_ids.reserve(context_info.actions.size());
+
+    int edit_offset = 0;
+    for (auto &p : context_info.actions)
+    {
+      auto hash = add_action(&payload[p.first], p.second);
+      action_ids.push_back(hash);
+      std::stringstream replacement;
+      replacement << "{\"__aid\":";
+      replacement << hash << "}";
+      edited_payload.replace(p.first - edit_offset, p.second, replacement.str());
+
+      //adjust the edit position based on how many bytes were deleted.
+      edit_offset += p.second - replacement.tellp();
+    }
+
+    return error_code::success;
+  }
+} // namespace reinforcement_learning
---
+++ unit_test/dedup_test.cc
@@ -0,0 +1,94 @@
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#define BOOST_TEST_MODULE Main
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include "dedup.h"
+
+namespace r = reinforcement_learning;
+namespace err = reinforcement_learning::error_code;
+
+BOOST_AUTO_TEST_CASE(dedup_remove_unknown_action)
+{
+  r::dedup_dict dict;
+  BOOST_CHECK_EQUAL(false, dict.remove_action(1));
+}
+
+BOOST_AUTO_TEST_CASE(dedup_add_remove_action)
+{
+  r::dedup_dict dict;
+  auto id = dict.add_action("abc", 3);
+  auto id2 = dict.add_action("abc", 3);
+
+  BOOST_CHECK_EQUAL(id, id2);
+
+  BOOST_CHECK_EQUAL(true, dict.remove_action(id));
+  BOOST_CHECK_EQUAL(true, dict.remove_action(id));
+  BOOST_CHECK_EQUAL(false, dict.remove_action(id));
+}
+
+BOOST_AUTO_TEST_CASE(dedup_add_empty_action)
+{
+  r::dedup_dict dict;
+  auto id = dict.add_action("", 0);
+  BOOST_CHECK_EQUAL(true, dict.remove_action(id));
+}
+
+BOOST_AUTO_TEST_CASE(dedup_add_get_action)
+{
+  r::dedup_dict dict;
+  auto id = dict.add_action("{abc}", 5);
+  auto str = dict.get_action(id);
+
+  BOOST_CHECK_EQUAL(5, str.size());
+  BOOST_CHECK_EQUAL("{abc}", str.to_string());
+}
+
+BOOST_AUTO_TEST_CASE(dedup_bad_json)
+{
+  r::dedup_dict dict;
+  const char *payload = "{ invalid }";
+  std::string p_out;
+  r::dedup_dict::action_list_t a_out;
+
+  BOOST_CHECK_EQUAL(err::json_parse_error, dict.transform_payload_and_add_actions(payload, p_out, a_out, nullptr));
+}
+
+BOOST_AUTO_TEST_CASE(dedup_simple_json)
+{
+  r::dedup_dict dict;
+  std::string payload = R"(
+    {
+      "s_": "1",
+      "_multi": [
+        { "b_": "1" },
+        { "b_": "2" }
+      ],
+      "_slots": [ { "a": 10 }, { "b": 20 } ]
+    })";
+
+  std::string p_out;
+  r::dedup_dict::action_list_t a_out;
+
+  BOOST_CHECK_EQUAL(err::success, dict.transform_payload_and_add_actions(payload.c_str(), p_out, a_out, nullptr));
+  BOOST_CHECK_EQUAL(2, a_out.size());
+  BOOST_CHECK_EQUAL(989852256, a_out[0]);
+  BOOST_CHECK_EQUAL(178626470, a_out[1]);
+
+  std::string transformed_payload = R"(
+    {
+      "s_": "1",
+      "_multi": [
+        {"__aid":989852256},
+        {"__aid":178626470}
+      ],
+      "_slots": [ { "a": 10 }, { "b": 20 } ]
+    })";
+  BOOST_CHECK_EQUAL(transformed_payload, p_out);
+
+  BOOST_CHECK_EQUAL(true, dict.remove_action(989852256));
+  BOOST_CHECK_EQUAL(true, dict.remove_action(178626470));
+  BOOST_CHECK_EQUAL(false, dict.remove_action(989852256));
+  BOOST_CHECK_EQUAL(false, dict.remove_action(178626470));
+}
