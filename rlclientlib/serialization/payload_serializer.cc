586b1f32e7d78ec7bd8d163026b2fad8405f04fa
Rodrigo Kumpera <kumpera@users.noreply.github.com>
2020-11-23 12:27:47 -0500

    Fix windows build and a couple of warnings pointed by VS. (#232)

--- rlclientlib/serialization/payload_serializer.cc
+++ rlclientlib/serialization/payload_serializer.cc
@@ -4,7 +4,6 @@ namespace reinforcement_learning {
   using namespace messages::flatbuff;
   namespace logger {
     int get_learning_mode(learning_mode mode_in, v2::LearningModeType& mode_out, api_status* status) {
-      v2::LearningModeType result;
       switch (mode_in) {
         case APPRENTICE: mode_out = v2::LearningModeType_Apprentice; return error_code::success;
         case ONLINE: mode_out = v2::LearningModeType_Online; return error_code::success;
