22408809aba1be256119ad508fc36c4c38559626
Olga Vrousgou <olvrousg@microsoft.com>
2020-12-17 22:41:13 +0000

    echo path

--- .scripts/build.cmd
+++ .scripts/build.cmd
@@ -1,3 +1,5 @@
+ECHO %PATH%
+
 @ECHO OFF
 IF DEFINED DebugBuildScripts (
     @ECHO ON
@@ -11,8 +13,6 @@ PUSHD %~dp0
 REM TODO: Figure out how to parametrize this script?! (is there a standard, or do we actually need parse args?)
 ECHO Building "%rlRoot%\rl.sln" for Release x64
 "%msbuildPath%" /verbosity:normal /m /p:Configuration=Release;Platform=x64 "%rlRoot%\rl.sln"
-
-POPD
 
 POPD
 
