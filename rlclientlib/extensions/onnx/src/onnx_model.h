845dbafadeb752c50501e83799e5eb0c8a516bde
Jacob Alber <jacob.alber@microsoft.com>
2021-01-12 10:22:01 -0500

    fixup: naming for use_unstructured_input

--- rlclientlib/extensions/onnx/src/onnx_model.h
+++ rlclientlib/extensions/onnx/src/onnx_model.h
@@ -16,7 +16,7 @@ namespace reinforcement_learning { namespace onnx {
     onnx_model& operator=(onnx_model&&) = delete;
 
   public:
-    onnx_model(i_trace* trace_logger, const char* app_id, const char* output_name, bool parse_feature_string);
+    onnx_model(i_trace* trace_logger, const char* app_id, const char* output_name, bool use_unstructured_input);
     int update(const model_management::model_data& data, bool& model_ready, api_status* status = nullptr) override;
     int choose_rank(uint64_t rnd_seed, const char* features, std::vector<int>& action_ids, std::vector<float>& action_pdf, std::string& model_version, api_status* status = nullptr) override;
 
