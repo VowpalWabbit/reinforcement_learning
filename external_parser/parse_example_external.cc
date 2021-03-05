83618acc4a8d2e39619fdfdfdab5815487cc3f13
Olga Vrousgou <olgavrou@gmail.com>
2021-03-05 12:43:08 +0000

    add arg parser

--- external_parser/parse_example_external.cc
+++ external_parser/parse_example_external.cc
@@ -2,18 +2,32 @@
 // individual contributors. All rights reserved. Released under a BSD (revised)
 // license as described in the file LICENSE.
 
+#include "parse_args.h"
 #include "parse_example_binary.h"
+
+static size_t constexpr BINARY_PARSER_VERSION = 1;
 
 namespace VW {
 namespace external {
 
+bool parser_options::is_enabled() { return binary; }
+
 std::unique_ptr<parser>
-parser::get_external_parser(vw *all, const std::string &parser_type) {
-  if (parser_type == "binary") {
+parser::get_external_parser(vw *all, const input_options &parsed_options) {
+  if (parsed_options.ext_opts->binary) {
     return VW::make_unique<binary_parser>(all);
   }
-  throw std::runtime_error("parser_type specified is not recognised: " +
-                           parser_type);
+  throw std::runtime_error("parser_type specified is not recognised");
+}
+
+void parser::set_parse_args(VW::config::option_group_definition &in_options,
+                            input_options &parsed_options) {
+  parsed_options.ext_opts = VW::make_unique<parser_options>();
+  in_options.add(
+      VW::config::make_option("binary_parser", parsed_options.ext_opts->binary)
+          .help("data file will be interpreted using the binary parser "
+                "version: " +
+                std::to_string(BINARY_PARSER_VERSION)));
 }
 
 parser::~parser() {}
--- external_parser/parse_example_external.h
+++ external_parser/parse_example_external.h
@@ -4,16 +4,25 @@
 
 #pragma once
 #include "memory.h"
+#include "options.h"
 #include "vw.h"
 
 namespace VW {
 namespace external {
 
+struct parser_options {
+  bool is_enabled();
+  bool binary;
+};
+
 int parse_examples(vw *all, v_array<example *> &examples);
 
 class parser {
 public:
-  static std::unique_ptr<parser> get_external_parser(vw *all, const std::string &parser_type);
+  static std::unique_ptr<parser>
+  get_external_parser(vw *all, const input_options &parsed_options);
+  static void set_parse_args(VW::config::option_group_definition &in_options,
+                             input_options &parsed_options);
   virtual ~parser();
   virtual bool parse_examples(vw *all, v_array<example *> &examples) = 0;
 };
