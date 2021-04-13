6274592434c6495fe9ea37d5ac80ce2d4ed6f2c2
Olga Vrousgou <olgavrou@gmail.com>
2021-04-13 12:12:54 +0100

    add another dep

--- .scripts/restore-vw-bp-deps.cmd
+++ .scripts/restore-vw-bp-deps.cmd
@@ -8,11 +8,13 @@ SETLOCAL
 CALL %~dp0init.cmd
 PUSHD %~dp0
 
+%vcpkgPath% install zlib:x64-windows
 %vcpkgPath% install flatbuffers:x64-windows
 %vcpkgPath% install boost-filesystem:x64-windows
 %vcpkgPath% install boost-thread:x64-windows
 %vcpkgPath% install boost-program-options:x64-windows
 %vcpkgPath% install boost-test:x64-windows
+%vcpkgPath% install boost-align:x64-windows
 
 POPD
 
