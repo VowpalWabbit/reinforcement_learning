9aecafe3d3619d979c3bdadb1bb997d4c61f4d6c
Olga Vrousgou <olvrousg@microsoft.com>
2021-04-13 18:10:01 +0100

    statically link vcpkg deps

--- .scripts/build-vw-bp.cmd
+++ .scripts/build-vw-bp.cmd
@@ -10,7 +10,9 @@ PUSHD %~dp0
 PUSHD %vwBinaryParserRoot%
 
 ECHO Building %vwBinaryParserRoot% for Release x64
-cmake . -DCMAKE_TOOLCHAIN_FILE=%VcpkgCmake% -Wno-deprecated -DVCPKG_TARGET_TRIPLET=x64-windows -G "Visual Studio 15 2017" -A x64 -DBUILD_FLATBUFFERS=OFF -DCMAKE_CONFIGURATION_TYPES="Release" -DWARNINGS=OFF -DWARNINGS=OFF -DWARNING_AS_ERROR=OFF -DDO_NOT_BUILD_VW_C_WRAPPER=OFF  -DBUILD_JAVA=OFF -DBUILD_PYTHON=OFF -DBUILD_TESTS=OFF -DBUILD_EXPERIMENTAL_BINDING=OFF
+REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
+REM using -Wno-deprecated because zstd's CMakeLists.txt produces a deprecated warning that causes the CI to fail
+cmake . -DCMAKE_TOOLCHAIN_FILE=%VcpkgCmake% -Wno-deprecated -DVCPKG_TARGET_TRIPLET=x64-windows-static-md -G "Visual Studio 15 2017" -A x64 -DCMAKE_CONFIGURATION_TYPES="Release"
 "%msbuildPath%" /verbosity:normal /m /p:Configuration=Release;Platform=x64 "%vwBinaryParserRoot%\vw_binary_parser.sln"
 
 POPD
