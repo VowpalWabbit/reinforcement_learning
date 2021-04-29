576046f6203667a211d46dc02adf3fe8fb63cc35
Olga Vrousgou <olgavrou@gmail.com>
2021-04-29 18:40:39 +0100

    static cast

--- external_parser/timestamp_helper.cc
+++ external_parser/timestamp_helper.cc
@@ -9,7 +9,7 @@ timestamp_to_chrono(const v2::TimeStamp &ts) {
   // chrono::time_point expressed in days
   // date::sys_days can have hours/mins/seconds/nanosecond added to it and the
   // result will be a chrono::time_point expressed in nanoseconds
-  return std::chrono::time_point<std::chrono::system_clock>(
+  return static_cast<std::chrono::time_point<std::chrono::system_clock>>(
       date::sys_days(date::year(ts.year()) / date::month(ts.month()) /
                      date::day(ts.day())) +
       std::chrono::hours(ts.hour()) + std::chrono::minutes(ts.minute()) +
