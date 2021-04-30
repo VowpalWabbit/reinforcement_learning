8b152e0ace46361a4f7be8caec272f6d9478af59
Olga Vrousgou <olgavrou@gmail.com>
2021-04-30 11:30:44 +0100

    use chrono builtin type

--- external_parser/timestamp_helper.h
+++ external_parser/timestamp_helper.h
@@ -6,7 +6,6 @@
 #include <chrono>
 
 namespace v2 = reinforcement_learning::messages::flatbuff::v2;
-using TimePoint =
-    std::chrono::time_point<std::chrono::system_clock,
-                            std::chrono::duration<int64_t, std::nano>>;
+using TimePoint = std::chrono::time_point<std::chrono::system_clock,
+                                          std::chrono::nanoseconds>;
 TimePoint timestamp_to_chrono(const v2::TimeStamp &ts);
