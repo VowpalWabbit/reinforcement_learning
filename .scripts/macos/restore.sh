3ed4675ac939247ae3674b1680f3a7fb9bdbd889
Olga Vrousgou <olgavrou@gmail.com>
2021-05-20 15:29:14 +0100

    brew install with raw-url

--- .scripts/macos/restore.sh
+++ .scripts/macos/restore.sh
@@ -6,4 +6,6 @@ set -x
 brew update
 brew upgrade
 
-brew install cmake boost cpprestsdk flatbuffers@1.10.0 openssl
+brew install cmake boost cpprestsdk openssl
+# install flatbuffers 1.10.0
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/050eed3eee550b0489c66fa9d6ca5991baf9268e/Formula/flatbuffers.rb
