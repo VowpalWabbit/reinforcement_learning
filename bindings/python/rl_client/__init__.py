7c23b8fccb3462b3ab8147dd621595376abab5d1
Jack Gerrits <jackgerrits@users.noreply.github.com>
2021-05-27 14:22:24 -0400

    Python binding overhaul, automatic binary and documentation generation (#330)
    * WIP python bindings
    
    * rl static deps
    
    * update args
    
    * update ci
    
    * update build image
    
    * remove other platforms for now
    
    * add docs section
    
    * Write migration guide
    
    * rename from main to py_api.cc
    
    * remove swig pieces
    
    * migrate examples
    
    * add build to ignore
    
    * More updates
    
    * Remove unnecessary bits of conf
    
    * remove duplicated add_subdirectory
    
    * Default static deps to off
    
    * test ci
    
    * fetch submodules
    
    * fix doc gen
    
    * Update build_python_wheels.yml
    
    * Update build_python_wheels.yml
    
    * Fix examples
    
    * Support additional cmake args

--- bindings/python/rl_client/__init__.py
+++
@@ -1,5 +0,0 @@
-from .rl_client import *
-
-# Bring exception definitions into global module namespace so they can be accessed as rl_client.json_parse_error_exception
-for name, value in rl_client._rl_client.rl_exception_type_dictionary.items():
-    globals()[name] = value
