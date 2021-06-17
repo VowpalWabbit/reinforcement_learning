74f4ba8f297f83106ab3ce9bc478585cbbe7e271
Alexey Taymanov <ataymano@gmail.com>
2021-06-17 19:34:44 -0400

    int -> float

--- external_parser/event_processors/joined_event.h
+++ external_parser/event_processors/joined_event.h
@@ -89,7 +89,7 @@ struct cb_joined_event : public typed_joined_event {
     int index = interaction_data.actions[0];
     auto action = interaction_data.actions[0];
     auto probability = interaction_data.probabilities[0];
-    auto weight = 1 / (1.f - interaction_data.probabilityOfDrop);
+    auto weight = 1.f / (1.f - interaction_data.probabilityOfDrop);
 
     examples[index]->l.cb.costs.push_back({0.f, action, probability});
     examples[index]->l.cb.weight = weight;
