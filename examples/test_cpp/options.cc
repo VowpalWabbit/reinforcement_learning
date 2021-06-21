a24a4ac820735ec6995e450c6f550434bf0339d6
zwd-ms <71728747+zwd-ms@users.noreply.github.com>
2021-06-21 13:05:52 -0400

    Add multistep loops to testcpp. (#352)
    * Fix multithreading.
    
    * Refactor to introduce multistep loop kind.
    
    * Add multistep loop to testcpp.

---
+++ examples/test_cpp/options.cc
@@ -0,0 +1,48 @@
+#include "options.h"
+
+#include <boost/program_options.hpp>
+#include <iostream>
+
+namespace po = boost::program_options;
+
+bool is_help(const po::variables_map& vm) {
+  return vm.count("help") > 0;
+}
+
+po::variables_map process_cmd_line(const int argc, char** argv) {
+  po::options_description desc("Options");
+  desc.add_options()
+    ("help,h", "produce help message")
+    ("json_config,j", po::value<std::string>()->
+      default_value("client.json"), "JSON file with config information for hosted RL loop")
+    ("threads,t", po::value<size_t>()->default_value(1), "Number of threads per instance")
+    ("examples,n", po::value<size_t>()->default_value(10), "Number of examples per thread")
+	("features,x", po::value<size_t>()->default_value(10), "Features count")
+    ("actions,a", po::value<size_t>()->default_value(2), "Number of actions")
+    ("experiment_name,e", po::value<std::string>()->required(), "(REQUIRED) experiment name")
+    ("float_outcome,f", "if outcome is float (otherwise - json)")
+    ("sleep,s", po::value<size_t>()->default_value(0), "Milliseconds to sleep between loop iterations")
+    ("duration,d", po::value<size_t>(), "Duration of experiment (in ms). Alternative to n")
+    ("instances,i", po::value<size_t>()->default_value(1), "Number of test loop instances")
+    ("reward_period,r", po::value<size_t>()->default_value(0), "Ratio period (0 - no reward, otherwise - every $reward_period interaction is receiving reward)")
+    ("slots,q", po::value<size_t>()->default_value(0), "Number of slots (ccb simulation is running if > 0)")
+    ("episode_length,m", po::value<size_t>()->default_value(0), "Length of an episode (running multistep if > 0)")
+    ;
+
+  po::variables_map vm;
+  store(parse_command_line(argc, argv, desc), vm);
+
+  if (is_help(vm))
+    std::cout << desc << std::endl;
+
+  return vm;
+}
+
+void throw_if_conflicting(const po::variables_map& vm, const std::string& first, const std::string& second) {
+  if (vm.count(first) && !vm[first].defaulted() &&
+    vm.count(second) && !vm[second].defaulted())
+  {
+    throw std::logic_error(std::string("Conflicting options '") +
+      first + "' and '" + second + "'.");
+  }
+}
--- examples/test_cpp/test_loop.h
+++ examples/test_cpp/test_loop.h
@@ -1,16 +1,23 @@
 #pragma once
+
+#include <memory>
+#include <boost/program_options.hpp>
+
 #include "experiment_controller.h"
 #include "test_data_provider.h"
 #include "live_model.h"
 
-#include <memory>
-#include <boost/program_options.hpp>
+enum class LoopKind {
+  CB,
+  CCB,
+  MULTISTEP,
+};
 
 class test_loop {
 public:
   test_loop(size_t index, const boost::program_options::variables_map& vm);
   bool init();
-  void run(bool is_ccb);
+  void run() const;
 
 private:
   int load_file(const std::string& file_name, std::string& config_str) const;
@@ -18,19 +25,22 @@ private:
     reinforcement_learning::utility::configuration& config,
     reinforcement_learning::api_status* status) const;
   std::string generate_experiment_name(const std::string& experiment_name_base,
-	  size_t threads, size_t features, size_t actions, size_t slots, size_t index);
+	  size_t threads, size_t features, size_t actions, size_t slots, size_t episode_length, size_t index) const;
+  LoopKind get_loop_kind(const boost::program_options::variables_map& vm) const;
 
-  void cb_loop(size_t thread_id);
-  void ccb_loop(size_t thread_id);
+  void cb_loop(size_t thread_id) const;
+  void ccb_loop(size_t thread_id) const;
+  void multistep_loop(size_t thread_id) const;
 
 private:
+  const LoopKind loop_kind;
   const size_t threads;
-  std::unique_ptr<experiment_controller> controller;
   const size_t sleep_interval;
+  const size_t episode_length;
   const std::string experiment_name;
   const std::string json_config;
   test_data_provider test_inputs;
-
+  std::vector<std::unique_ptr<experiment_controller>> controllers;
   std::vector<std::shared_ptr<std::ofstream>> loggers;
   std::unique_ptr<reinforcement_learning::live_model> rl;
 };
