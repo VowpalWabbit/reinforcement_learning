ceb4c429b76c523712f9c145f1a5f3a76192094c
Wangda Zhang <wangdazhang@microsoft.com>
2021-07-13 14:55:17 -0400

    Fix episodic python binding.

--- examples/python/basic_usage.py
+++ examples/python/basic_usage.py
@@ -47,17 +47,16 @@ def basic_usage_cb():
 def basic_usage_multistep():
     config = load_config_from_json("client.json")
 
-    model = rl_client.live_model(config)
-    model.init()
+    model = rl_client.LiveModel(config)
 
-    episode1 = rl_client.episode_state("episode1")
-    episode2 = rl_client.episode_state("episode2")
+    episode1 = rl_client.EpisodeState("episode1")
+    episode2 = rl_client.EpisodeState("episode2")
 
     # episode1, event1
     context1 = '{"shared":{"F1": 1.0}, "_multi": [{"AF1": 2.0}, {"AF1": 3.0}]}'
     response1 = model.request_episodic_decision(
         "event1", None, context1, episode1)
-    print("episode id:", episode1.get_episode_id())
+    print("episode id:", episode1.episode_id)
     print("event id:", response1.event_id)
     print("chosen action:", response1.chosen_action_id)
 
@@ -65,7 +64,7 @@ def basic_usage_multistep():
     context1 = '{"shared":{"F2": 1.0}, "_multi": [{"AF2": 2.0}, {"AF2": 3.0}]}'
     response1 = model.request_episodic_decision(
         "event1", None, context1, episode2)
-    print("episode id:", episode2.get_episode_id())
+    print("episode id:", episode2.episode_id)
     print("event id:", response1.event_id)
     print("chosen action:", response1.chosen_action_id)
 
@@ -73,7 +72,7 @@ def basic_usage_multistep():
     context2 = '{"shared":{"F1": 4.0}, "_multi": [{"AF1": 2.0}, {"AF1": 3.0}]}'
     response2 = model.request_episodic_decision(
         "event2", "event1", context2, episode1)
-    print("episode id:", episode1.get_episode_id())
+    print("episode id:", episode1.episode_id)
     print("event id:", response2.event_id)
     print("chosen action:", response2.chosen_action_id)
 
@@ -81,12 +80,12 @@ def basic_usage_multistep():
     context2 = '{"shared":{"F2": 4.0}, "_multi": [{"AF2": 2.0}, {"AF2": 3.0}]}'
     response2 = model.request_episodic_decision(
         "event2", "event1", context2, episode2)
-    print("episode id:", episode2.get_episode_id())
+    print("episode id:", episode2.episode_id)
     print("event id:", response2.event_id)
     print("chosen action:", response2.chosen_action_id)
 
-    model.report_outcome(episode1.get_episode_id(), "event1", 1.0)
-    model.report_outcome(episode2.get_episode_id(), "event2", 1.0)
+    model.report_outcome(episode1.episode_id, "event1", 1.0)
+    model.report_outcome(episode2.episode_id, "event2", 1.0)
 
 
 if __name__ == "__main__":
