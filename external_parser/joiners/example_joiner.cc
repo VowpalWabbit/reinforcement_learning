bcd34fab69877166892f34510e38e20aeac16cd0
Olga Vrousgou <olgavrou@gmail.com>
2021-07-19 19:11:26 +0100

    minor binary parser cleanup

--- external_parser/joiners/example_joiner.cc
+++ external_parser/joiners/example_joiner.cc
@@ -13,6 +13,7 @@
 // VW headers
 #include "parse_example_json.h"
 #include "parser.h"
+#include "scope_exit.h"
 
 example_joiner::example_joiner(vw *vw)
     : _vw(vw), _reward_calculation(&reward::earliest), _binary_to_json(false) {}
@@ -419,13 +420,23 @@ bool example_joiner::process_joined(v_array<example *> &examples) {
     }
   }
 
+  // this scope exit guard will execute when this method returns
+  // that way we can guarantee clean-up no matter where the return happens
+  // without having to duplicate the cleanup code
+  bool clear_examples = false;
+  auto clear_event_id_on_exit = VW::scope_exit([&] {
+    clear_event_id_batch_info(id);
+    if (clear_examples) {
+      clear_vw_examples(examples);
+    }
+  });
+
   if (_batch_grouped_examples.find(id) == _batch_grouped_examples.end()) {
     // can't learn from this interaction
     VW::io::logger::log_warn("Events with event id [{}] were processed but "
                              "no valid interaction found. Skipping..",
                              id);
-    clear_event_id_batch_info(id);
-    clear_vw_examples(examples);
+    clear_examples = true;
     return false;
   }
 
@@ -436,8 +447,7 @@ bool example_joiner::process_joined(v_array<example *> &examples) {
         "Interaction with event id [{}] has been invalidated due to malformed "
         "observation. Skipping...",
         id);
-    clear_event_id_batch_info(id);
-    clear_vw_examples(examples);
+    clear_examples = true;
     return false;
   }
 
@@ -450,15 +460,13 @@ bool example_joiner::process_joined(v_array<example *> &examples) {
     if (_binary_to_json) {
       log_converter::build_cb_json(_outfile, je);
 
-      clear_event_id_batch_info(id);
-      clear_vw_examples(examples);
+      clear_examples = true;
       return true;
     }
 
     if (!je.is_joined_event_learnable()) {
       _joiner_metrics.number_of_skipped_events++;
-      clear_event_id_batch_info(id);
-      clear_vw_examples(examples);
+      clear_examples = true;
       return true;
     }
   } else if (je.interaction_metadata.payload_type == v2::PayloadType_CCB) {
@@ -475,7 +483,6 @@ bool example_joiner::process_joined(v_array<example *> &examples) {
     examples.back()->is_newline = true;
   }
 
-  clear_event_id_batch_info(id);
   return true;
 }
 
