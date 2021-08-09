81ed919acc821f80d516eedff4161e0c823d939d
Alexey Taymanov <ataymano@gmail.com>
2021-08-09 08:07:17 -0400

    multistep deferred episodes test data

--- test_tools/log_parser/test_gen.py
+++ test_tools/log_parser/test_gen.py
@@ -43,6 +43,32 @@ class Env:
                 mk_multistep_payload(_episode_id='ep2', _event_id='2', _previous_id='1', _ctx=ctx2_2),
                 mk_outcome(_primary_id='ep2', _value = 3)])
 
+    def multistep_3_deferred_episodes(self):
+        ctx1_1 = """{"A": {"f": 1}, "_multi":[{"a1":"f1"},{"a2":"f2"}]}"""
+        ctx1_2 = """{"B": {"f": 1}, "_multi":[{"a1":"f1"},{"a2":"f2"}]}"""
+        ctx2_1 = """{"C": {"f": 1}, "_multi":[{"a1":"f1"},{"a2":"f2"}]}"""
+        ctx2_2 = """{"D": {"f": 1}, "_multi":[{"a1":"f1"},{"a2":"f2"}]}"""
+        ctx3_1 = """{"E": {"f": 1}, "_multi":[{"a1":"f1"},{"a2":"f2"}]}"""
+        ctx3_2 = """{"F": {"f": 1}, "_multi":[{"a1":"f1"},{"a2":"f2"}]}"""
+        with BinLogWriter(self.valid_joined_logs.joinpath('multistep_3_deferred_episodes.fb')) as writer:
+            writer.write_file_magic()
+            writer.write_checkpoint_info()
+            writer.write_regular_message([
+                mk_multistep_payload(_episode_id='ep1', _event_id='1', _ctx=ctx1_1, _deferred=True),
+                mk_multistep_payload(_episode_id='ep1', _event_id='2', _previous_id='1', _ctx=ctx1_2, _deferred=True),
+                mk_outcome(_primary_id='ep1', _secondary_id='1', _value = 2),
+                mk_outcome(_primary_id='ep1', _secondary_id='1', _value = None)]),                
+            writer.write_regular_message([
+                mk_multistep_payload(_episode_id='ep2', _event_id='1', _ctx=ctx2_1, _deferred=True),
+                mk_multistep_payload(_episode_id='ep2', _event_id='2', _previous_id='1', _ctx=ctx2_2, _deferred=True),
+                mk_outcome(_primary_id='ep2', _value = 3),
+                mk_outcome(_primary_id='ep2', _value = None)]),  
+            writer.write_regular_message([
+                mk_multistep_payload(_episode_id='ep3', _event_id='1', _ctx=ctx3_1, _deferred=True),
+                mk_multistep_payload(_episode_id='ep3', _event_id='2', _previous_id='1', _ctx=ctx3_2, _deferred=True),
+                mk_outcome(_primary_id='ep3', _value = 4)])
+                
+
 
 #%% Generate pdrop test data
 def main():
@@ -50,6 +76,7 @@ def main():
     env.cb_pdrop_05()
     env.cb_pdrop_1()
     env.multistep_2_episodes()
+    env.multistep_3_deferred_episodes()
 
 if __name__ == "__main__":
     main()
