015c3c7705ea0b7b6d0f6f4783b6fa28de6e6a16
Lahabar Sheetal Madhukar <lamadh@microsoft.com>
2021-09-16 13:09:31 -0700

    eventhub changes back in

--- rlclientlib/utility/eventhub_http_authorization.h
+++ rlclientlib/utility/eventhub_http_authorization.h
@@ -1,27 +1,30 @@
 #pragma once
 
 #include "api_status.h"
+#include "configuration.h"
+#include "constants.h"
 
+#include <cpprest/http_headers.h>
 #include <chrono>
 #include <mutex>
 #include <string>
 
+using namespace web::http;
+
 namespace reinforcement_learning {
   class i_trace;
 
-  // The eventhub_client send string data in POST requests to an HTTP endpoint.
-  // It handles authorization headers specific for the Azure event hubs.
-  class http_authorization {
+  class eventhub_http_authorization {
   public:
-    http_authorization(const std::string& host, const std::string& key_name,
-      const std::string& key, const std::string& name, i_trace* trace);
-    ~http_authorization() = default;
-    
-    int init(api_status* status);
-    int get(std::string& authorization, api_status* status);
+    eventhub_http_authorization() = default;
+    ~eventhub_http_authorization() = default;
+
+    int init(const utility::configuration& config, api_status* status, i_trace* trace);
+    int get_http_headers(http_headers& headers, api_status* status);
 
   private:
     int check_authorization_validity_generate_if_needed(api_status* status);
+    int get_authorization_token(std::string& authorization, api_status* status);
 
     static int generate_authorization_string(
       std::chrono::seconds now,
@@ -35,17 +38,17 @@ namespace reinforcement_learning {
       i_trace* trace);
 
     // cannot be copied or assigned
-    http_authorization(const http_authorization&) = delete;
-    http_authorization(http_authorization&&) = delete;
-    http_authorization& operator=(const http_authorization&) = delete;
-    http_authorization& operator=(http_authorization&&) = delete;
+    eventhub_http_authorization(const eventhub_http_authorization&) = delete;
+    eventhub_http_authorization(eventhub_http_authorization&&) = delete;
+    eventhub_http_authorization& operator=(const eventhub_http_authorization&) = delete;
+    eventhub_http_authorization& operator=(eventhub_http_authorization&&) = delete;
 
   private:
-    const std::string _eventhub_host; //e.g. "ingest-x2bw4dlnkv63q.servicebus.windows.net"
-    const std::string _shared_access_key_name; //e.g. "RootManageSharedAccessKey"
-    const std::string _shared_access_key;
+    std::string _eventhub_host; //e.g. "ingest-x2bw4dlnkv63q.servicebus.windows.net"
+    std::string _shared_access_key_name; //e.g. "RootManageSharedAccessKey"
+    std::string _shared_access_key;
     //e.g. Check https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-authentication-and-security-model-overview
-    const std::string _eventhub_name; //e.g. "interaction"
+    std::string _eventhub_name; //e.g. "interaction"
 
     std::string _authorization;
     long long _valid_until; //in seconds
