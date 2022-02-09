0b00dc36c2fea31f1eae88cf3f0ecc52f4a32a6b
Tejaswi Paruchuri <tparuchuri@microsoft.com>
2022-02-09 09:29:36 -0800

    fix build

--- rlclientlib/utility/header_authorization.cc
+++ rlclientlib/utility/header_authorization.cc
@@ -2,7 +2,7 @@
 #include <codecvt>
 
 using namespace utility;
-using convert_t = std::codecvt_utf8<char_t>;
+using convert_t = std::codecvt_utf8<wchar_t>;
 
 namespace reinforcement_learning {
   int header_authorization::init(const utility::configuration& config, api_status* status, i_trace* trace) {
@@ -11,8 +11,13 @@ namespace reinforcement_learning {
       RETURN_ERROR(trace, status, http_api_key_not_provided);
     }
     _api_key = api_key;
+    #ifdef _WIN32
     std::wstring_convert<convert_t, wchar_t> strconverter;
     _http_api_header_key_name = strconverter.from_bytes(config.get(name::HTTP_API_HEADER_KEY_NAME, value::HTTP_API_DEFAULT_HEADER_KEY_NAME));
+    #endif
+    #ifndef _WIN32
+    _http_api_header_key_name = config.get(name::HTTP_API_HEADER_KEY_NAME, value::HTTP_API_DEFAULT_HEADER_KEY_NAME);
+    #endif
     //_http_api_header_key_name = boost::locale::conv::utf_to_utf<char_t>(config.get(name::HTTP_API_HEADER_KEY_NAME, value::HTTP_API_DEFAULT_HEADER_KEY_NAME));
     return error_code::success;
   }
