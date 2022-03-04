d8356c54f49e7d3a45b8e804dd1d34a4188de908
Griffin Bassman <griffinbassman@gmail.com>
2022-03-04 10:44:08 -0500

    add windoows vw cmake flags

--- .scripts/build-vw-bp.cmd
+++ .scripts/build-vw-bp.cmd
@@ -12,7 +12,7 @@ PUSHD %vwBinaryParserRoot%
 ECHO Building %vwBinaryParserRoot% for Release x64
 REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
 REM using -Wno-deprecated because zstd's CMakeLists.txt produces a deprecated warning that causes the CI to fail
-cmake . -DCMAKE_TOOLCHAIN_FILE=%VcpkgCmake% -Wno-deprecated -DVCPKG_TARGET_TRIPLET=x64-windows-static-md -G "Visual Studio 15 2017" -A x64 -DCMAKE_CONFIGURATION_TYPES="Release" -DWARNING_AS_ERROR=OFF -DWARNINGS=OFF
+cmake . -DCMAKE_TOOLCHAIN_FILE=%VcpkgCmake% -Wno-deprecated -DVCPKG_TARGET_TRIPLET=x64-windows-static-md -G "Visual Studio 15 2017" -A x64 -DCMAKE_CONFIGURATION_TYPES="Release" -DWARNING_AS_ERROR=OFF -DWARNINGS=OFF -DSTATIC_LINK_BINARY_PARSER=ON -DFMT_SYS_DEP=ON -DSPDLOG_SYS_DEP=ON
 "%msbuildPath%" /verbosity:normal /m /p:Configuration=Release;Platform=x64 "%vwBinaryParserRoot%\vw_binary_parser.sln"
 
 POPD
