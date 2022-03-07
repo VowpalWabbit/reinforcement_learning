e06c048f5c1731836153e4588261fc2cb5a49ff0
Olga Vrousgou <olgavrou@gmail.com>
2022-03-07 16:56:20 -0500

    try static

--- .scripts/restore-vw-bp-deps.cmd
+++ .scripts/restore-vw-bp-deps.cmd
@@ -9,14 +9,14 @@ CALL %~dp0init.cmd
 PUSHD %~dp0
 
 REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
-%vcpkgPath% install zlib:x64-windows-static-md
-%vcpkgPath% install flatbuffers:x64-windows-static-md
-%vcpkgPath% install boost-filesystem:x64-windows-static-md
-%vcpkgPath% install boost-thread:x64-windows-static-md
-%vcpkgPath% install boost-program-options:x64-windows-static-md
-%vcpkgPath% install boost-test:x64-windows-static-md
-%vcpkgPath% install boost-align:x64-windows-static-md
-%vcpkgPath% install boost-math:x64-windows-static-md
+%vcpkgPath% install zlib:x64-windows-static
+%vcpkgPath% install flatbuffers:x64-windows-static
+%vcpkgPath% install boost-filesystem:x64-windows-static
+%vcpkgPath% install boost-thread:x64-windows-static
+%vcpkgPath% install boost-program-options:x64-windows-static
+%vcpkgPath% install boost-test:x64-windows-static
+%vcpkgPath% install boost-align:x64-windows-static
+%vcpkgPath% install boost-math:x64-windows-static
 
 POPD
 
