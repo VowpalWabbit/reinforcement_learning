46daaeba640b7c5cda7c1eea791bcc42770de634
Griffin Bassman <griffinbassman@gmail.com>
2022-03-08 09:27:47 -0500

    remove static md

--- .scripts/restore-vw-bp-deps.cmd
+++ .scripts/restore-vw-bp-deps.cmd
@@ -8,15 +8,14 @@ SETLOCAL
 CALL %~dp0init.cmd
 PUSHD %~dp0
 
-REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
-%vcpkgPath% install zlib:x64-windows-static-md
-%vcpkgPath% install flatbuffers:x64-windows-static-md
-%vcpkgPath% install boost-filesystem:x64-windows-static-md
-%vcpkgPath% install boost-thread:x64-windows-static-md
-%vcpkgPath% install boost-program-options:x64-windows-static-md
-%vcpkgPath% install boost-test:x64-windows-static-md
-%vcpkgPath% install boost-align:x64-windows-static-md
-%vcpkgPath% install boost-math:x64-windows-static-md
+%vcpkgPath% install zlib:x64-windows
+%vcpkgPath% install flatbuffers:x64-windows
+%vcpkgPath% install boost-filesystem:x64-windows
+%vcpkgPath% install boost-thread:x64-windows
+%vcpkgPath% install boost-program-options:x64-windows
+%vcpkgPath% install boost-test:x64-windows
+%vcpkgPath% install boost-align:x64-windows
+%vcpkgPath% install boost-math:x64-windows
 
 POPD
 
