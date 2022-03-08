ce8cdb27fb65b34aeed0c8127d73f30442ce12e8
Griffin Bassman <griffinbassman@gmail.com>
2022-03-08 10:12:16 -0500

    static

--- .scripts/restore-vw-bp-deps.cmd
+++ .scripts/restore-vw-bp-deps.cmd
@@ -8,14 +8,14 @@ SETLOCAL
 CALL %~dp0init.cmd
 PUSHD %~dp0
 
-%vcpkgPath% install zlib:x64-windows
-%vcpkgPath% install flatbuffers:x64-windows
-%vcpkgPath% install boost-filesystem:x64-windows
-%vcpkgPath% install boost-thread:x64-windows
-%vcpkgPath% install boost-program-options:x64-windows
-%vcpkgPath% install boost-test:x64-windows
-%vcpkgPath% install boost-align:x64-windows
-%vcpkgPath% install boost-math:x64-windows
+%vcpkgPath% install zlib:x64-windows-static
+%vcpkgPath% install flatbuffers:x64-windows-static
+%vcpkgPath% install boost-filesystem:x64-windows-static
+%vcpkgPath% install boost-thread:x64-windows-static
+%vcpkgPath% install boost-program-options:x64-windows-static
+%vcpkgPath% install boost-test:x64-windows-static
+%vcpkgPath% install boost-align:x64-windows-static
+%vcpkgPath% install boost-math:x64-windows-static
 
 POPD
 
