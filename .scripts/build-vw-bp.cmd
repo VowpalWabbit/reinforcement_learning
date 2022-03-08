623f4209359d38a516ec35c2a80da4438e2c801b
Jack Gerrits <jackgerrits95@gmail.com>
2022-03-08 10:38:29 -0500

    remove md

--- .scripts/build-vw-bp.cmd
+++ .scripts/build-vw-bp.cmd
@@ -12,7 +12,7 @@ ECHO Building %vwBinaryParserRoot% for Release x64
 REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
 REM using -Wno-deprecated because zstd's CMakeLists.txt produces a deprecated warning that causes the CI to fail
 
-cmake -S external_parser -B build -DCMAKE_TOOLCHAIN_FILE=%VcpkgCmake% -Wno-deprecated -DVCPKG_TARGET_TRIPLET="x64-windows-static-md" -G "Visual Studio 15 2017" -A x64 -DWARNING_AS_ERROR=OFF -DWARNINGS=OFF
+cmake -S external_parser -B build -DCMAKE_TOOLCHAIN_FILE=%VcpkgCmake% -Wno-deprecated -DVCPKG_TARGET_TRIPLET="x64-windows-static" -G "Visual Studio 15 2017" -A x64 -DWARNING_AS_ERROR=OFF -DWARNINGS=OFF
 cmake --build build --config Release --target vw-bin binary_parser_unit_tests
 
 POPD
--- .scripts/restore-vw-bp-deps.cmd
+++ .scripts/restore-vw-bp-deps.cmd
@@ -8,7 +8,7 @@ SETLOCAL
 CALL %~dp0init.cmd
 
 REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
-%vcpkgPath% install --triplet=x64-windows-static-md zlib flatbuffers boost-filesystem boost-thread boost-program-options boost-test boost-align boost-math
+%vcpkgPath% install --triplet=x64-windows-static zlib flatbuffers boost-filesystem boost-thread boost-program-options boost-test boost-align boost-math
 
 POPD
 
