b7205ee977555955fc1ebe56d62e44f118551ce1
Olga Vrousgou <olgavrou@gmail.com>
2022-03-08 13:52:01 -0500

    .

--- .scripts/restore-vw-bp-deps.cmd
+++ .scripts/restore-vw-bp-deps.cmd
@@ -9,7 +9,7 @@ CALL %~dp0init.cmd
 PUSHD %~dp0
 
 REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
-%vcpkgPath% install --host-triplet=x64-windows-static --triplet=x64-windows-static zlib flatbuffers boost-filesystem boost-thread boost-program-options boost-test boost-align boost-math
+%vcpkgPath% install --host-triplet=x64-windows-static-md --triplet=x64-windows-static-md zlib flatbuffers boost-filesystem boost-thread boost-program-options boost-test boost-align boost-math
 
 POPD
 
