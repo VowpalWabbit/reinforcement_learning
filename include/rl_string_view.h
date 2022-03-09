4b08994e4e71af9458f55cddf8e631625870e840
Jack Gerrits <jack@jackgerrits.com>
2022-03-09 09:51:34 -0500

    rely on vw target transitive include

--- include/rl_string_view.h
+++ include/rl_string_view.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Rely on the VW implementation of string_view
-#include "../ext_libs/vowpal_wabbit/vowpalwabbit/vw_string_view.h"
+#include "vw_string_view.h"
 
 namespace reinforcement_learning
 {
