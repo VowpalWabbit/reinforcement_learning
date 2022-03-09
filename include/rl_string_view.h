91de68fd6cfc6a413274c6da16f846ed762908ad
Olga Vrousgou <olgavrou@gmail.com>
2022-03-09 00:14:36 -0500

    revert the revert

--- include/rl_string_view.h
+++ include/rl_string_view.h
@@ -1,18 +1,9 @@
 #pragma once
 
-#include "hashstring.h"
-#include <boost/version.hpp>
+// Rely on the VW implementation of string_view
+#include "../ext_libs/vowpal_wabbit/vowpalwabbit/vw_string_view.h"
 
-#if BOOST_VERSION < 106100
-#include <boost/utility/string_ref.hpp>
 namespace reinforcement_learning
 {
-using string_view = boost::string_ref;
+using string_view = VW::string_view;
 }
-#else
-#include <boost/utility/string_view.hpp>
-namespace reinforcement_learning
-{
-using string_view = boost::string_view;
-}
-#endif
