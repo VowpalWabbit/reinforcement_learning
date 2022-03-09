00f32a47dcd517ce4436cef113a42ea3e7ffac4a
Jack Gerrits <jack@jackgerrits.com>
2022-03-09 13:35:51 -0500

    different flatbuf tool paths

--- .scripts/build-vw-bp.cmd
+++ .scripts/build-vw-bp.cmd
@@ -9,6 +9,19 @@ CALL %~dp0init.cmd
 PUSHD %~dp0
 PUSHD %vwBinaryParserRoot%
 
+SET "flatcPath=%VCPKG_INSTALLATION_ROOT%\installed\x64-windows-static-md-v141\tools\flatbuffers\flatc.exe"
+
+REM The correct way to integrate this is to use msbuild targets and contribute them back to flatbuff,
+REM rather than a command line execution
+IF NOT DEFINED flatcPath (
+    IF NOT DEFINED VcpkgInstallRoot (
+        ECHO ERROR: flatcPath is not configured, but VcpkgInstallRoot is also not configured. Cannot find vcpkg.
+        EXIT /b 1
+    )
+
+    SET "flatcPath=%VcpkgInstallRoot%installed\x64-windows-static-md-v141\tools\flatbuffers\flatc.exe"
+)
+
 ECHO Building %vwBinaryParserRoot% for Release x64
 REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
 REM using -Wno-deprecated because zstd's CMakeLists.txt produces a deprecated warning that causes the CI to fail
--- .scripts/init.cmd
+++ .scripts/init.cmd
@@ -45,18 +45,6 @@ IF NOT DEFINED VCPKG_INSTALLATION_ROOT (
 SET "vcpkgPath=%VCPKG_INSTALLATION_ROOT%\vcpkg.exe"
 SET "VcpkgIntegration=%VCPKG_INSTALLATION_ROOT%\scripts\buildsystems\msbuild\vcpkg.targets"
 SET "VcpkgCmake=%VCPKG_INSTALLATION_ROOT%\scripts\buildsystems\vcpkg.cmake"
-SET "flatcPath=%VCPKG_INSTALLATION_ROOT%\installed\x64-windows-v141\tools\flatbuffers\flatc.exe"
-
-REM The correct way to integrate this is to use msbuild targets and contribute them back to flatbuff,
-REM rather than a command line execution
-IF NOT DEFINED flatcPath (
-    IF NOT DEFINED VcpkgInstallRoot (
-        ECHO ERROR: flatcPath is not configured, but VcpkgInstallRoot is also not configured. Cannot find vcpkg.
-        EXIT /b 1
-    )
-
-    SET "flatcPath=%VcpkgInstallRoot%installed\x64-windows-v141\tools\flatbuffers\flatc.exe"
-)
 
 REM Repo-specific paths
 IF NOT DEFINED rlRoot (
