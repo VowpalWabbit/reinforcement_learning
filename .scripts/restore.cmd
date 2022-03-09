bc4d98febd6f792b24961d8c0882334332fb15c4
Griffin Bassman <griffinbassman@gmail.com>
2022-03-09 10:25:34 -0500

    vcpkg deps in restore

--- .scripts/restore.cmd
+++ .scripts/restore.cmd
@@ -8,9 +8,7 @@ SETLOCAL
 CALL %~dp0init.cmd
 PUSHD %~dp0
 
-%vcpkgPath% install cpprestsdk:x64-windows
-%vcpkgPath% install flatbuffers:x64-windows
-%vcpkgPath% install openssl-windows:x64-windows
+%vcpkgPath% install --host-triplet=x64-windows --triplet=x64-windows cpprestsdk openssl-windows zlib flatbuffers boost-filesystem boost-thread boost-program-options boost-test boost-align boost-math
 
 REM TODO: This really should be out-of-source (also, can we switch to vcpkg for these?)
 ECHO Restoring "%rlRoot%\ext_libs\vowpal_wabbit\vowpalwabbit\packages.config"
