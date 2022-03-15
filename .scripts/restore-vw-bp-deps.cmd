308073853b5fc4186fd133b16adc09291dfc82c6
Jack Gerrits <jackgerrits@users.noreply.github.com>
2022-03-15 13:34:21 -0700

    ci: Migrate Windows CI to 2019 (#463)
    * Use windows 2019
    
    * fix quotes
    
    * change path
    
    * add custom triplet
    
    * update target sdk
    
    * add missing sdk change
    
    * update bp ci
    
    * set host triplet
    
    * set host triplet
    
    * change flatc path
    
    * different flatbuf tool paths
    
    * set property on command line
    
    * try migrating to netcoreapp3.1
    
    * reset cache
    
    * debug find
    
    * add search path
    
    * Update restore.cmd

--- .scripts/restore-vw-bp-deps.cmd
+++ .scripts/restore-vw-bp-deps.cmd
@@ -6,10 +6,10 @@ IF DEFINED DebugBuildScripts (
 SETLOCAL
 
 CALL %~dp0init.cmd
-PUSHD %~dp0
+PUSHD %~dp0\..\
 
 REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
-%vcpkgPath% install --host-triplet=x64-windows-static-md --triplet=x64-windows-static-md zlib flatbuffers boost-filesystem boost-thread boost-program-options boost-test boost-align boost-math
+%vcpkgPath% install "--overlay-triplets=custom-triplets" --triplet=x64-windows-static-md-v141 --host-triplet=x64-windows-static-md-v141 zlib flatbuffers boost-filesystem boost-thread boost-program-options boost-test boost-align boost-math
 
 POPD
 
