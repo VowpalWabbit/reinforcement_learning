1829d12caf8f5b60768ef9989d47fe6b1b6bff49
Griffin Bassman <griffinbassman@gmail.com>
2022-03-15 12:31:11 -0400

    boost-uuid

--- .scripts/restore.cmd
+++ .scripts/restore.cmd
@@ -8,7 +8,7 @@ SETLOCAL
 CALL %~dp0init.cmd
 PUSHD %~dp0
 
-%vcpkgPath% install --host-triplet=x64-windows --triplet=x64-windows cpprestsdk openssl-windows zlib flatbuffers boost-filesystem boost-thread boost-program-options boost-test boost-align boost-math
+%vcpkgPath% install --host-triplet=x64-windows --triplet=x64-windows cpprestsdk openssl-windows zlib flatbuffers boost-filesystem boost-thread boost-program-options boost-test boost-align boost-math boost-uuid
 
 REM TODO: This really should be out-of-source (also, can we switch to vcpkg for these?)
 ECHO Restoring "%rlRoot%\ext_libs\vowpal_wabbit\vowpalwabbit\packages.config"
