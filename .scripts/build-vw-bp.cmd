a70677774ceebc529f58881e0c4e37f3bd2817ec
Jack Gerrits <jack@jackgerrits.com>
2022-03-29 12:47:16 -0400

    update build

--- .scripts/build-vw-bp.cmd
+++ .scripts/build-vw-bp.cmd
@@ -12,8 +12,8 @@ PUSHD %vwBinaryParserRoot%
 ECHO Building %vwBinaryParserRoot% for Release x64
 REM x64-windows-static-md is a community triplet that sets CRT to dynamic but all the other dependencies to static
 REM using -Wno-deprecated because zstd's CMakeLists.txt produces a deprecated warning that causes the CI to fail
-cmake . -DCMAKE_TOOLCHAIN_FILE=%VcpkgCmake% -Wno-deprecated -DVCPKG_TARGET_TRIPLET=x64-windows-static-md-v141 -G "Visual Studio 16 2019" -A x64 -T v141 -DCMAKE_CONFIGURATION_TYPES="Release" -DWARNING_AS_ERROR=OFF -DWARNINGS=OFF
-if %errorlevel% neq 0 exit /b %errorlevel%
+cmake -S . -B build -A x64 -DCMAKE_TOOLCHAIN_FILE=%VcpkgCmake% -DVCPKG_TARGET_TRIPLET=x64-windows-static-md-v141 -DWARNING_AS_ERROR=OFF -Wno-deprecated
+if %errorlevel% neq 0 exit /b %errorlevel%-DVCPKG_TARGET_TRIPLET=x64-windows-static-md-v141
 
 cmake --build build --config Release -t vw-bin binary_parser_unit_tests
 if %errorlevel% neq 0 exit /b %errorlevel%
