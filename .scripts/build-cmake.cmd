19d1e423742d6b4438be7ff83f4a6980ebf73ecd
Jack Gerrits <jackgerrits@users.noreply.github.com>
2022-03-30 09:37:51 -0700

    Migrate CI to GH Actions, update flatbuffer finding logic, enable macos BP CI (#470)
    * ci: add testing workflow
    
    * be in right dir for install
    
    * update integration, add valgrind, get win install working first
    
    * build and test win
    
    * try dotnet install
    
    * fix path
    
    * add config
    
    * change fb modules
    
    * fallback to old find method
    
    * update find
    
    * specify add_test name and command
    
    * use scripts in workflow
    
    * fixes
    
    * build valgrind
    
    * build
    
    * revert changes
    
    * add newlien
    
    * add bp to build
    
    * add module path hack
    
    * remove old CIs, fix module path
    
    * fix fb for bp
    
    * spelling mistake
    
    * try exit on fail
    
    * update build
    
    * try different find
    
    * change casing
    
    * remove file
    
    * change casing
    
    * quote path
    
    * fix init
    
    * fix test

---
+++ .scripts/build-cmake.cmd
@@ -0,0 +1,18 @@
+@ECHO OFF
+IF DEFINED DebugBuildScripts (
+    @ECHO ON
+)
+
+SETLOCAL
+
+CALL %~dp0init-cmake.cmd
+PUSHD %rlRoot%
+
+cmake -S . -B build -A "x64" -DCMAKE_TOOLCHAIN_FILE="%VcpkgCmake%" -DVCPKG_TARGET_TRIPLET=x64-windows-v141
+if %errorlevel% neq 0 exit /b %errorlevel%
+cmake --build build --config Release
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+POPD
+
+ENDLOCAL
--- .scripts/test-cmake.cmd
+++ .scripts/test-cmake.cmd
@@ -6,12 +6,9 @@ IF DEFINED DebugBuildScripts (
 SETLOCAL
 
 CALL %~dp0init-cmake.cmd
-PUSHD %vwBinaryParserRoot%
+PUSHD %rlRoot%\build
 
-ECHO Running vw binary parser unit tests
 ctest --verbose --output-on-failure -C Release
 if %errorlevel% neq 0 exit /b %errorlevel%
-
-POPD
 
 ENDLOCAL
