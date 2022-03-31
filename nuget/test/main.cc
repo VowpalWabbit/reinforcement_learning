dbe0360ae6dc7e6c1e2df069c9d4c327fff238c6
Griffin Bassman <griffinbassman@gmail.com>
2022-03-31 15:19:45 -0400

    update to add include directory

--- nuget/test/main.cc
+++ nuget/test/main.cc
@@ -1,19 +1,10 @@
-#include "rlclientlib/ranking_response.h"
-#include "rlclientlib/explore.h"
+#include "learning_mode.h"
+#include "constants.h"
 #include <assert.h>
-
-const int NUM_ACTIONS = 10;
-namespace e = exploration;
-namespace r = reinforcement_learning;
 
 int main()
 {
-  const float epsilon = 0.2f;
-  const auto top_action_id = 0;
-  float pdf[NUM_ACTIONS];
-  auto scode = e::generate_epsilon_greedy(epsilon, top_action_id, pdf, pdf + NUM_ACTIONS);
-  assert(scode == 0);
-  uint32_t chosen_index;
-  scode = e::sample_after_normalizing(7791, pdf, pdf + NUM_ACTIONS, chosen_index);
-  assert(scode == 0);
+  const char* apprentice = "apprentice";
+  auto mode = reinforcement_learning::learning::to_learning_mode(apprentice);
+  assert(mode == reinforcement_learning::APPRENTICE);
 }
