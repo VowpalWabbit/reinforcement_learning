2b505b997300813d59f0f4228537f64f9f2bb5c5
Griffin Bassman <griffinbassman@gmail.com>
2022-04-21 12:44:57 -0400

    unique_ptr main

--- nuget/test/main.cc
+++ nuget/test/main.cc
@@ -2,7 +2,7 @@
 
 int main()
 {
-  //const reinforcement_learning::utility::configuration config;
-  //reinforcement_learning::live_model m(config, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+  reinforcement_learning::utility::configuration config;
+  std::unique_ptr<reinforcement_learning::live_model> _rl = std::unique_ptr<reinforcement_learning::live_model>(new reinforcement_learning::live_model(config, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr));
   return 0;
 }
