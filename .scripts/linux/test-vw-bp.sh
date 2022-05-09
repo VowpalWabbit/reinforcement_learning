5564fe78a02018fa9b4396b01540387fd0bb587d
Jack Gerrits <jack@jackgerrits.com>
2022-05-09 16:36:34 -0400

    update path

--- .scripts/linux/test-vw-bp.sh
+++ .scripts/linux/test-vw-bp.sh
@@ -12,7 +12,7 @@ cd $REPO_DIR/external_parser/build
 
 ctest --verbose --output-on-failure
 
-cd vw_binary_parser/vowpalwabbit
+cd ext_libs/vowpal_wabbit/vowpalwabbit/cli/
 
 ./vw --extra_metrics metrics.json -d ../../../unit_tests/test_files/valid_joined_logs/cb_simple.log --binary_parser --cb_explore_adf
 
