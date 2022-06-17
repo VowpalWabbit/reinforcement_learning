45d4a3e76aefc4c1f983031cc565be4c0cd0b13c
Byron Xu <byronxu@microsoft.com>
2022-06-17 16:41:10 -0400

    Fix more clang-tidy errors

--- .scripts/linux/run-clang-tidy.sh
+++ .scripts/linux/run-clang-tidy.sh
@@ -8,18 +8,22 @@ cd "$REPO_DIR"
 
 # -DCMAKE_EXPORT_COMPILE_COMMANDS=On is manually set because in CMake 3.16, just setting it in the CMakeLists.txt does not work.
 cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=On
-cd build
+
+# generate flatbuffers files
+cmake --build build --target fbgenerator_v1
+cmake --build build --target fbgenerator_v2
 
 # check that compile_commands.json was generated
+cd build
 if [[ ! -f compile_commands.json ]]; then
     echo "Error: CMake did not successfully generate compile_commands.json"
     exit 1
 fi
 
-# remove entries in compile_commands.json referencing ext_libs
+# remove entries in compile_commands.json referencing ext_libs 
 jq 'map(select(.directory | contains("ext_libs") or contains("unit_test") | not))' compile_commands.json > compile_commands_without_ext_libs.json
 mv compile_commands_without_ext_libs.json compile_commands.json
+cd ..
 
 # run clang-tidy on the modified compile_commands.json
-cd ..
 run-clang-tidy -p build
