e0d5c191d18769d83d5ba95e4f41d8e3e210bf02
Byron Xu <byronxu@microsoft.com>
2022-06-28 16:10:05 -0400

    Get CMake to install libraries used by rlclientlib

--- ext_libs/ext_libs.cmake
+++ ext_libs/ext_libs.cmake
@@ -1,16 +1,28 @@
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/fakeit)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/date)
-if (RL_BUILD_PYTHON)
-    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/pybind11)
+
+if(RL_BUILD_PYTHON)
+  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/pybind11)
 endif()
 
-set(VW_INSTALL OFF CACHE BOOL "")
 set(BUILD_TESTING OFF CACHE BOOL "")
 set(BUILD_JAVA OFF CACHE BOOL "")
 set(BUILD_PYTHON OFF CACHE BOOL "")
 set(BUILD_DOCS OFF CACHE BOOL "")
 set(WARNINGS OFF CACHE BOOL "")
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/vowpal_wabbit EXCLUDE_FROM_ALL)
-if (USE_ZSTD)
+
+# Nuget build relies on installing libraries with cmake --install
+# This means that subdirectories cannot be EXCLUDE_FROM_ALL
+if(RL_BUILD_NUGET)
+  set(VW_INSTALL ON CACHE BOOL "" FORCE)
+  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/vowpal_wabbit)
+  if(USE_ZSTD)
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zstd/build/cmake)
+  endif()
+else()
+  set(VW_INSTALL OFF CACHE BOOL "")
+  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/vowpal_wabbit EXCLUDE_FROM_ALL)
+  if(USE_ZSTD)
     add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zstd/build/cmake EXCLUDE_FROM_ALL)
+  endif()
 endif()
