95359ca9a05433a461dd5fc31e1ff64b3f985cd8
Byron Xu <byronxu@microsoft.com>
2022-07-01 13:15:31 -0400

    Update nuget build scripts

--- nuget/test/main.cc
+++ nuget/test/main.cc
@@ -1,6 +1,8 @@
 #include "live_model.h"
 #include "config_utility.h"
 
+#include <iostream>
+
 namespace r = reinforcement_learning;
 namespace u = reinforcement_learning::utility;
 
@@ -8,5 +10,7 @@ int main()
 {
   u::configuration config;
   auto rl = std::make_unique<r::live_model>(config);
+
+  std::cout << "Successfully ran rlclientlib test program" << std::endl;
   return 0;
 }
