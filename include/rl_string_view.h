49eb2e091be04465e0df512f67f3f0f66ea2eaf4
Byron Xu <byronxu@microsoft.com>
2022-07-08 17:49:25 -0400

    Always use nonstd::string_view to maintain consistency with VW

--- include/rl_string_view.h
+++ include/rl_string_view.h
@@ -1,17 +1,9 @@
 #pragma once
 
-#include "future_compat.h"
+#include "nonstd/string_view.h"
 
-#ifdef HAS_STD17
-#  include <string_view>
-namespace reinforcement_learning
-{
-using std::string_view;
-}
-#else
-#  include "nonstd/string_view.h"
 namespace reinforcement_learning
 {
 using nonstd::string_view;
+
 }
-#endif
