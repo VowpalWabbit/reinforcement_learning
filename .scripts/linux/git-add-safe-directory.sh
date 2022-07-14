f1b396d50eb7054630834a5d1ab983123f0ed6d7
Byron Xu <byronxu@microsoft.com>
2022-07-14 15:24:45 -0400

    Don't do git add safe directory *

--- .scripts/linux/git-add-safe-directory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Fix git "unsafe repository" error
-# https://github.com/actions/checkout/issues/766
-# https://stackoverflow.com/questions/71855882/how-to-add-directory-recursively-on-git-safe-directory
-git config --global --add safe.directory '*'
