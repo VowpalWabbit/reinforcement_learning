e22cac80159639f6cbb576b8d21ce59b5d1e79a3
Peter Chang <petchang@microsoft.com>
2022-08-16 10:38:51 -0400

    Change episodic enable conditions

--- rlclientlib/live_model_impl.cc
+++ rlclientlib/live_model_impl.cc
@@ -518,7 +518,11 @@ int live_model_impl::init_loggers(api_status* status)
 
   // TODO: Use a specific episode message type (for now it is the same with the observation logger, using
   // observation_logger_facade).
-  if (_configuration.get(name::EPISODE_SENDER_IMPLEMENTATION, nullptr) != nullptr)
+  if (
+    _configuration.get(name::EPISODE_EH_HOST, nullptr) != nullptr
+      ||
+    _configuration.get(name::EPISODE_FILE_NAME, nullptr) != nullptr
+  )
   {
     // Get the name of raw data (as opposed to message) sender for episodes.
     const auto* const episode_sender_impl =
