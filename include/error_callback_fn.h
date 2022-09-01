783a511eb76a6aa605ca95edbca03fde855772cf
Byron Xu <byronxu@microsoft.com>
2022-09-01 17:32:36 -0400

    For callbacks, use std::function instead of function pointers

--- include/error_callback_fn.h
+++ include/error_callback_fn.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <functional>
 #include <mutex>
 
 namespace reinforcement_learning
@@ -14,11 +15,39 @@ class api_status;
 class error_callback_fn
 {
 public:
+  using callback_t = std::function<void(const api_status&)>;
   using error_fn = void (*)(const api_status&, void*);
-  void set(error_fn, void*);
+  void set(error_fn fn, void* context)
+  {
+    std::lock_guard<std::mutex> lock(_mutex);
+    if (fn != nullptr) { _fn = std::bind(fn, std::placeholders::_1, context); }
+    else
+    {
+      _fn = nullptr;
+    }
+  }
+
+  template <typename ContextT>
+  void set(void (*fn)(const api_status&, ContextT*), ContextT* context)
+  {
+    std::lock_guard<std::mutex> lock(_mutex);
+    if (fn != nullptr) { _fn = std::bind(fn, std::placeholders::_1, context); }
+    else
+    {
+      _fn = nullptr;
+    }
+  }
+
   void report_error(api_status& s);
 
-  error_callback_fn(error_fn, void*);
+  error_callback_fn(error_fn fn, void* context) { set(fn, context); }
+
+  template <typename ContextT>
+  error_callback_fn(void (*fn)(const api_status&, ContextT*), ContextT* context)
+  {
+    set<ContextT>(fn, context);
+  }
+
   ~error_callback_fn() = default;
 
   error_callback_fn(const error_callback_fn&) = delete;
@@ -28,7 +57,6 @@ public:
 
 private:
   std::mutex _mutex;
-  error_fn _fn;
-  void* _context;
+  callback_t _fn;
 };
 }  // namespace reinforcement_learning
--- rlclientlib/error_callback_fn.cc
+++ rlclientlib/error_callback_fn.cc
@@ -4,28 +4,16 @@ using namespace std;
 
 namespace reinforcement_learning
 {
-void error_callback_fn::set(error_fn fn, void* context)
-{
-  lock_guard<mutex> lock(_mutex);
-  _fn = fn;
-  _context = context;
-}
-
-// Function pointer may have been typecasted
-// Disable UBSan here to prevent generating an error
-#ifdef RL_USE_UBSAN
-__attribute__((no_sanitize("undefined")))
-#endif
 void error_callback_fn::report_error(api_status& s)
 {
-  if (_fn == nullptr) { return; }
+  if (!_fn) { return; }
 
   lock_guard<mutex> lock(_mutex);
-  if (_fn != nullptr)
+  if (_fn)
   {
     try
     {
-      _fn(s, _context);
+      _fn(s);
     }
     catch (...)
     {
@@ -34,5 +22,4 @@ void error_callback_fn::report_error(api_status& s)
   }
 }
 
-error_callback_fn::error_callback_fn(error_fn fn, void* cntxt) : _fn(fn), _context(cntxt) {}
 }  // namespace reinforcement_learning
