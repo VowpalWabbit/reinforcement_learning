afe071797234fa69e24c0332cad20fd05e5d4dc9
Peter Chang <petchang@microsoft.com>
2022-09-20 11:33:18 -0400

    cleaning up scripts

--- test_tools/e2e_testing/compare_serialized_examples.py
+++ test_tools/e2e_testing/compare_serialized_examples.py
@@ -1,28 +1,9 @@
 #! /usr/bin/env python3 -W ignore::DeprecationWarning
-import argparse
-import flatbuffers
-import numpy as np
-import os
-import zstd
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--base_dir", type=str, help="Base files location"
-)
-parser.add_argument(
-    "--compare_dir", help="Comparison files location"
-)
-parser.add_argument(
-    "--flatc_dir", default=None, help="flatc executable directory"
-)
-args = parser.parse_args()
-flatc_file = 'flatc'
-if args.flatc_dir is not None:
-    flatc_file = os.path.join(args.flatc_dir, 'flatc')
 try:
     import reinforcement_learning.messages.flatbuff.v2.EventBatch
 except Exception as e:
     import pathlib
-    import subprocess   
+    import subprocess
 
     script_dir = pathlib.Path(__file__).parent.absolute()
     input_dir = (
@@ -32,7 +13,7 @@ except Exception as e:
 
     input_files = " ".join([str(x) for x in input_dir.glob("*.fbs")])
     subprocess.run(
-        f"{flatc_file} --python {input_files}", cwd=script_dir, shell=True, check=True
+        f"flatc --python {input_files}", cwd=script_dir, shell=True, check=True
     )
 
 
@@ -62,8 +43,14 @@ from reinforcement_learning.messages.flatbuff.v2.JoinedPayload import *
 from reinforcement_learning.messages.flatbuff.v2.CheckpointInfo import *
 from reinforcement_learning.messages.flatbuff.v2.ProblemType import *
 
+import argparse
+import flatbuffers
+import numpy as np
+import os
+import zstd
 
 PREAMBLE_LENGTH = 8
+
 
 class PreambleStreamReader:
     def __init__(self, file_name):
@@ -103,20 +90,36 @@ class PreambleStreamReader:
 
                 if m.PayloadType() == PayloadType.CB:
                     yield (m.PayloadType(), CbEvent.GetRootAsCbEvent(payload, 0))
-                elif m.PayloadType() == PayloadType.CCB or m.PayloadType() == PayloadType.Slates:
-                    yield (m.PayloadType(), MultiSlotEvent.GetRootAsMultiSlotEvent(payload, 0))
+                elif (
+                    m.PayloadType() == PayloadType.CCB
+                    or m.PayloadType() == PayloadType.Slates
+                ):
+                    yield (
+                        m.PayloadType(),
+                        MultiSlotEvent.GetRootAsMultiSlotEvent(payload, 0),
+                    )
                 elif m.PayloadType() == PayloadType.Outcome:
-                    yield (m.PayloadType(), OutcomeEvent.GetRootAsOutcomeEvent(payload, 0))
+                    yield (
+                        m.PayloadType(),
+                        OutcomeEvent.GetRootAsOutcomeEvent(payload, 0),
+                    )
                 elif m.PayloadType() == PayloadType.CA:
                     yield (m.PayloadType(), CaEvent.GetRootAsCaEvent(payload, 0))
                 elif m.PayloadType() == PayloadType.DedupInfo:
                     yield (m.PayloadType(), DedupInfo.GetRootAsDedupInfo(payload, 0))
                 elif m.PayloadType() == PayloadType.MultiStep:
-                    yield (m.PayloadType(), MultiStepEvent.GetRootAsMultiStepEvent(payload, 0))
+                    yield (
+                        m.PayloadType(),
+                        MultiStepEvent.GetRootAsMultiStepEvent(payload, 0),
+                    )
                 elif m.PayloadType() == PayloadType.Episode:
-                    yield (m.PayloadType(), EpisodeEvent.GetRootAsEpisodeEvent(payload, 0))
+                    yield (
+                        m.PayloadType(),
+                        EpisodeEvent.GetRootAsEpisodeEvent(payload, 0),
+                    )
                 else:
                     raise Exception("unknown payload type " + str(m.PayloadType()))
+
 
 # Similar hack to the C# one due to limited binding codegen
 def getString(table):
@@ -133,15 +136,21 @@ def cast(table, tmp_type):
     tmp.Init(table.Bytes, table.Pos)
     return tmp
 
+
 def compare_cb(base, compare):
-    raise Exception('cb comparison is unsupported')
+    raise Exception("cb comparison is unsupported")
+
 
 def compare_multislot(base, compare):
-    raise Exception('multislot comparison is unsupported')
+    raise Exception("multislot comparison is unsupported")
+
 
 def compare_outcome(base, compare):
-    if base.ValueType() != compare.ValueType() or base.IndexType() != compare.IndexType():
-        raise Exception('outcome type is different')
+    if (
+        base.ValueType() != compare.ValueType()
+        or base.IndexType() != compare.IndexType()
+    ):
+        raise Exception("outcome type is different")
 
     base_value = None
     compare_value = None
@@ -152,7 +161,7 @@ def compare_outcome(base, compare):
         base_value = cast(base.Value(), NumericOutcome).Value()
         compare_value = cast(compare.Value(), NumericOutcome).Value()
     if base_value != compare_value:
-        raise Exception('outcome value is different')
+        raise Exception("outcome value is different")
 
     base_index = None
     compare_index = None
@@ -163,47 +172,63 @@ def compare_outcome(base, compare):
         base_index = cast(base.Index(), NumericIndex).Index()
         compare_index = cast(compare.Index(), NumericIndex).Index()
     if base_index != compare_index:
-        raise Exception('outcome index is different')
+        raise Exception("outcome index is different")
 
     if base.ActionTaken() != compare.ActionTaken():
-        raise Exception('outcome action_taken is different')
+        raise Exception("outcome action_taken is different")
+
 
 def compare_ca(base, compare):
-    raise Exception('ca comparison is unsupported')
+    raise Exception("ca comparison is unsupported")
+
 
 def compare_dedup(base, compare):
-    raise Exception('dedup comparison is unsupported')
+    raise Exception("dedup comparison is unsupported")
+
 
 def compare_multistep(base, compare):
     if base.EventId() != compare.EventId():
-        raise Exception('Multistep event IDs do not match')
+        raise Exception("Multistep event IDs do not match")
     if base.PreviousId() != compare.PreviousId():
-        raise Exception('Multistep event IDs do not match')
+        raise Exception("Multistep event IDs do not match")
     if not np.array_equal(base.ActionIdsAsNumpy(), compare.ActionIdsAsNumpy()):
-        raise Exception('Multistep ActionIDs do not match')
+        raise Exception("Multistep ActionIDs do not match")
     if not np.array_equal(base.ContextAsNumpy(), compare.ContextAsNumpy()):
-        raise Exception('Multistep Contexts do not match')
+        raise Exception("Multistep Contexts do not match")
     if not np.array_equal(base.ProbabilitiesAsNumpy(), compare.ProbabilitiesAsNumpy()):
-        raise Exception('Multistep Probabilities do not match')
+        raise Exception("Multistep Probabilities do not match")
     if base.ModelId() != compare.ModelId():
-        raise Exception('Multistep model IDs do not match')
+        raise Exception("Multistep model IDs do not match")
     if base.DeferredAction() != compare.DeferredAction():
-        raise Exception('Multistep model IDs do not match')
+        raise Exception("Multistep model IDs do not match")
 
 
 def compare_episode(base, compare):
     if base.EpisodeId() != compare.EpisodeId():
-        raise Exception('Episode IDs do not match')
+        raise Exception("Episode IDs do not match")
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_dir", type=str, help="Base files location")
+    parser.add_argument("--compare_dir", help="Comparison files location")
+    args = parser.parse_args()
 
-
-    base_files = {f for f in os.listdir(args.base_dir) if os.path.isfile(os.path.join(args.base_dir, f))}
-    compare_files = {f for f in os.listdir(args.compare_dir) if os.path.isfile(os.path.join(args.compare_dir, f))}
+    base_files = {
+        f
+        for f in os.listdir(args.base_dir)
+        if os.path.isfile(os.path.join(args.base_dir, f))
+    }
+    compare_files = {
+        f
+        for f in os.listdir(args.compare_dir)
+        if os.path.isfile(os.path.join(args.compare_dir, f))
+    }
 
     if base_files != compare_files:
-        raise Exception("base_dir and compare_dir must contain the same number of files with the same names")
+        raise Exception(
+            "base_dir and compare_dir must contain the same number of files with the same names"
+        )
 
     for filename in base_files:
         print(f"comparing file {filename}")
@@ -212,7 +237,9 @@ def main():
 
         for base, compare in zip(base_reader.messages(), compare_reader.messages()):
             if base is None or compare is None:
-                raise Exception("base and compare files do not contain the same number of events")
+                raise Exception(
+                    "base and compare files do not contain the same number of events"
+                )
             if base[0] != compare[0]:
                 raise Exception("base and compare types are no the same")
             if base[0] == PayloadType.CB:
@@ -230,12 +257,10 @@ def main():
             elif base[0] == PayloadType.Episode:
                 compare_episode(base[1], compare[1])
             else:
-                raise Exception('Unknown type')
-
+                raise Exception("Unknown type")
 
     print("Comparison completed. All events match")
 
-        
 
 if __name__ == "__main__":
     main()
