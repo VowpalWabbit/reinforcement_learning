a26f838f5531b206da398c87ab8148b1e0a76e48
Peter Chang <petchang@microsoft.com>
2022-09-21 11:42:47 -0400

    forgot to add script

---
+++ test_tools/e2e_testing/evaluate_result.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3 -W ignore::DeprecationWarning
+
+from vw_executor.artifacts import Output
+
+import argparse
+import numpy as np
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_file", type=str, help="Base file location")
+    parser.add_argument("--compare_file", help="Comparison file location")
+    args = parser.parse_args()
+
+    vw_base = Output(args.base_file)
+    vw_compare = Output(args.compare_file)
+
+    if not np.isclose([vw_base.loss], [vw_compare.loss]):
+        raise Exception("Loss is different between base and compare")
+
+    if vw_base.loss_table.shape != vw_compare.loss_table.shape:
+        raise Exception("Base and compare loss tables are a different shape")
+
+    for i in range(len(vw_base.loss_table)):
+        if not np.isclose(vw_base.loss_table.iloc[i], vw_compare.loss_table.iloc[i]).all():
+            raise Exception("Loss is different between base and compare")
+
+    print("Outputs match")
+
+if __name__ == "__main__":
+    main()
