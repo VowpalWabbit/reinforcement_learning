2d945a79cc385147b9ed75573fbcb91d0063e50a
Jack Gerrits <jackgerrits@users.noreply.github.com>
2022-10-18 15:19:32 -0400

    feat: add joined log provider, update fed client interface (#519)

--- rlclientlib/federation/federated_client.h
+++ rlclientlib/federation/federated_client.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "api_status.h"
 #include "future_compat.h"
 #include "model_mgmt.h"
 
@@ -10,32 +11,13 @@
 namespace reinforcement_learning
 {
 /**
- * @brief Allows an implementation to be configurable based on what kinds of
- * aggregation payloads it supports.
- */
-enum class federated_aggregation_payload_t : uint32_t
-{
-  /// A delta containing save_resume info in VW format
-  vw_delta = 0,
-  // A full model containing save_resume info in VW format
-  vw_model,
-  /// A delta containing predict info only in VW format
-  vw_predict_only_delta,
-  /// A full model containing predict info only in VW format
-  vw_predict_only_model
-};
-
-/**
  * @brief This interface represents the fetching of new models and reporting of
  * local changes required for a federated learning system. This interface is not
  * stable and may still change.
  */
 struct i_federated_client
 {
-  i_federated_client(federated_aggregation_payload_t federated_aggregation_payload)
-      : _federated_aggregation_payload(federated_aggregation_payload)
-  {
-  }
+  virtual ~i_federated_client() = default;
 
   /**
    * @brief Try and get a new global model. There must be a matching call to
@@ -59,22 +41,11 @@ struct i_federated_client
    * for every successful call to try_get_model, and you cannot call
    * try_get_model again until report_result has been called.
    *
-   * @param payload payload represents the payload to aggregate. Will be
-   *                interpreted based on the return value of
-   *                get_aggregation_payload_type()
+   * @param payload payload represents the payload to aggregate. Must be a serialized VW model delta.
    * @param status Contains error information in the event of a failure
    * @returns Status code
    */
   RL_ATTR(nodiscard) virtual int report_result(const std::vector<uint8_t>& payload, api_status* status = nullptr) = 0;
-
-  /// The supplied payload to report_result must match the expected type.
-  RL_ATTR(nodiscard) federated_aggregation_payload_t get_expected_aggregation_payload_type() const noexcept
-  {
-    return _federated_aggregation_payload;
-  }
-
-private:
-  federated_aggregation_payload_t _federated_aggregation_payload;
 };
 
 }  // namespace reinforcement_learning
