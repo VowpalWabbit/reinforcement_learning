acdbc4c9f916617689e97525e975a5ffcdcdc788
Byron Xu <byronxu@microsoft.com>
2022-10-31 17:22:03 -0400

    Add another test for vw_joined_log_batch

--- unit_test/local_joiner_test.cc
+++ unit_test/local_joiner_test.cc
@@ -8,7 +8,7 @@
 
 using namespace reinforcement_learning;
 
-BOOST_AUTO_TEST_CASE(vw_joined_log_batch_test)
+BOOST_AUTO_TEST_CASE(vw_joined_log_batch_example_output)
 {
   auto opts = std::unique_ptr<VW::config::options_i>(new VW::config::options_cli(std::vector<std::string>{"--quiet"}));
   std::shared_ptr<VW::workspace> vw = VW::initialize_experimental(std::move(opts));
@@ -33,4 +33,41 @@ BOOST_AUTO_TEST_CASE(vw_joined_log_batch_test)
   // should get nullptr when empty
   BOOST_CHECK_EQUAL(batch.next_example(&example_out), error_code::success);
   BOOST_CHECK_EQUAL(example_out, nullptr);
+}
+
+BOOST_AUTO_TEST_CASE(vw_joined_log_batch_binary_output)
+{
+  auto opts = std::unique_ptr<VW::config::options_i>(new VW::config::options_cli(std::vector<std::string>{"--quiet"}));
+  std::shared_ptr<VW::workspace> vw = VW::initialize_experimental(std::move(opts));
+  vw_joined_log_batch batch(vw);
+
+  auto* ex = VW::read_example(*vw, "1 | a");
+  batch.add_example(ex);
+
+  // call next() to get binary buffer output
+  std::unique_ptr<VW::io::reader> buffer_out;
+  BOOST_CHECK_EQUAL(batch.next(buffer_out), error_code::success);
+
+  // re-create the example object
+  io_buf io_reader;
+  io_reader.add_file(std::move(buffer_out));
+  VW::multi_ex examples;
+  examples.push_back(VW::new_unused_example(*vw));
+  VW::read_example_from_cache(vw.get(), io_reader, examples);
+  BOOST_CHECK_EQUAL(examples.size(), 1);
+  BOOST_CHECK_NE(examples[0], nullptr);
+
+  // check some fields inside the examples to make sure they're equal
+  auto* ex_out = examples[0];
+  VW::setup_example(*vw, ex);
+  VW::setup_example(*vw, ex_out);
+  BOOST_CHECK_EQUAL(ex_out->num_features, ex->num_features);
+  BOOST_CHECK_EQUAL(ex_out->indices.size(), ex->indices.size());
+
+  // should get nullptr when empty
+  BOOST_CHECK_EQUAL(batch.next(buffer_out), error_code::success);
+  BOOST_CHECK_EQUAL(buffer_out.get(), nullptr);
+
+  // vw_joined_log_batch should automatically call finish_example on ex
+  vw->finish_example(*ex_out);
 }
