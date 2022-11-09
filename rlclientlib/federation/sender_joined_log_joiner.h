52fcd1ffd3ae6eab64e472e562e1dd12ff18d5e7
Jack Gerrits <jackgerrits95@gmail.com>
2022-11-09 17:57:13 -0500

    first rough, untested impl of local join

--- rlclientlib/federation/sender_joined_log_joiner.h
+++ rlclientlib/federation/sender_joined_log_joiner.h
@@ -7,6 +7,7 @@
 #include "future_compat.h"
 #include "generated/v2/Event_generated.h"
 #include "generated/v2/Metadata_generated.h"
+#include "generated/v2/FileFormat_generated.h"
 #include "joined_log_provider.h"
 #include "logger/message_type.h"
 #include "logger/preamble.h"
@@ -18,21 +19,49 @@
 
 #include <flatbuffers/buffer.h>
 #include <flatbuffers/detached_buffer.h>
+#include <flatbuffers/flatbuffer_builder.h>
 
 #include <chrono>
 #include <cstdint>
 #include <map>
+#include <memory>
 #include <ratio>
 #include <string>
 #include <vector>
 
 namespace reinforcement_learning
 {
-struct binary_joined_log_batch : public i_joined_log_batch
+
+  class buffer_reader : public VW::io::reader
 {
-  RL_ATTR(nodiscard)
-  int next(std::unique_ptr<VW::io::reader>& chunk_reader, api_status* status = nullptr) override {}
+public:
+  buffer_reader(std::vector<uint8_t>&& buffer)
+  : VW::io::reader(true),
+  _buffer(std::move(buffer)), _read_head(buffer.data())
+  {
+
+  }
+  ~buffer_reader() override = default;
+  ssize_t read(char* buffer, size_t num_bytes) override
+  {
+    num_bytes = std::min((_buffer.data() + _buffer.size()) - _read_head, static_cast<std::ptrdiff_t>(num_bytes));
+    if (num_bytes == 0) { return 0; }
+
+    std::memcpy(buffer, _read_head, num_bytes);
+    _read_head += num_bytes;
+
+    return num_bytes;
+  }
+  void reset() override
+  {
+    _read_head= _buffer.data();
+  }
+
+private:
+  std::vector<uint8_t> _buffer;
+  uint8_t* _read_head;
 };
+
 
 timestamp to_rl_timestamp(const reinforcement_learning::messages::flatbuff::v2::TimeStamp& ts)
 {
@@ -45,6 +74,39 @@ timestamp to_rl_timestamp(const reinforcement_learning::messages::flatbuff::v2::
   output.second = ts.second();
   output.sub_second = ts.subsecond();
   return output;
+}
+
+reinforcement_learning::messages::flatbuff::v2::TimeStamp from_rl_timestamp(const timestamp& ts)
+{
+    return reinforcement_learning::messages::flatbuff::v2::TimeStamp(ts.year, ts.month, ts.day, ts.hour, ts.minute, ts.second, ts.sub_second);
+}
+
+int emit_filemagic_message(std::vector<uint8_t>& output)
+{
+  // TODO consider doing this a safer way
+  uint32_t filemagic = 0x42465756;
+  output.reserve(output.size() + 4);
+  output.insert(std::end(output), reinterpret_cast<uint8_t*>(&filemagic), reinterpret_cast<uint8_t*>(&filemagic)+4);
+  return 0;
+}
+
+int emit_regular_message(std::vector<uint8_t>& output, flatbuffers::FlatBufferBuilder& fbb, flatbuffers::Offset<reinforcement_learning::messages::flatbuff::v2::JoinedPayload> joined_payload)
+{
+  // TODO consider doing this a safer way
+  uint32_t regular = 0xFFFFFFFF;
+  output.reserve(output.size() + 4);
+  output.insert(std::end(output), reinterpret_cast<uint8_t*>(&regular), reinterpret_cast<uint8_t*>(&regular)+4);
+
+  fbb.Finish(joined_payload);
+  auto buffer = fbb.Release();
+
+  uint32_t size = buffer.size();
+  output.reserve(output.size() + 4);
+  output.insert(std::end(output), reinterpret_cast<uint8_t*>(&size), reinterpret_cast<uint8_t*>(&size)+4);
+
+  output.reserve(output.size() + size);
+  output.insert(std::end(output), buffer.data(), buffer.data()+size);
+  return 0;
 }
 
 struct sender_joined_log_provider : public i_joined_log_provider, i_sender
@@ -63,16 +125,24 @@ struct sender_joined_log_provider : public i_joined_log_provider, i_sender
   }
 
   RL_ATTR(nodiscard)
-  int invoke_join(std::unique_ptr<i_joined_log_batch>& batch, api_status* status = nullptr) override {
+  int invoke_join(std::unique_ptr<VW::io::reader>& batch, api_status* status = nullptr) override {
+    std::lock_guard<std::mutex> lock(_mutex);
+    std::vector<uint8_t> result;
     const auto eud_cutoff = std::chrono::system_clock::now() - _eud_offset;
+    emit_filemagic_message(result);
     for (auto it = _interactions.cbegin(); it != _interactions.cend();)
     {
+      flatbuffers::FlatBufferBuilder fbb;
+      std::vector<flatbuffers::Offset<reinforcement_learning::messages::flatbuff::v2::JoinedEvent>> events;
+
       const auto& item = *it;
       const auto interaction_time = chrono_from_timestamp(std::get<0>(it->first));
       if (interaction_time <= eud_cutoff)
       {
         const auto reward_cutoff = interaction_time + _eud_offset;
-        // TODO write interaction to flatbuffer buffer
+
+        auto ts = from_rl_timestamp(std::get<0>(it->first));
+        events.push_back(reinforcement_learning::messages::flatbuff::v2::CreateJoinedEventDirect(fbb, &it->second, &ts));
         const auto event_id = std::get<1>(it->first);
         const auto& observations = _observations.find(event_id);
         if (observations != _observations.end())
@@ -82,7 +152,8 @@ struct sender_joined_log_provider : public i_joined_log_provider, i_sender
             auto observation_time = chrono_from_timestamp(std::get<0>(observation));
             if (observation_time <= reward_cutoff)
             {
-              // TODO write observation to flatbuffer buffer
+              auto ts = from_rl_timestamp(std::get<0>(observation));
+              events.push_back(reinforcement_learning::messages::flatbuff::v2::CreateJoinedEventDirect(fbb, &std::get<1>(observation), &ts));
             }
           }
           _observations.erase(event_id);
@@ -91,11 +162,18 @@ struct sender_joined_log_provider : public i_joined_log_provider, i_sender
       }
       else
       {
-        // TODO check that this works
+        // TODO make sure this is correct. As in does the timestamp ordering mean that all further events are within eud.
         // break;
         ++it;
       }
+
+      auto joined_payload = reinforcement_learning::messages::flatbuff::v2::CreateJoinedPayloadDirect(fbb, &events);
+      emit_regular_message(result, fbb, joined_payload);
     }
+
+    batch.reset(new buffer_reader(std::move(result)));
+
+    return 0;
   }
 
 protected:
