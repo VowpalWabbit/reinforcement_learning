3c7fb26821deded20f46fa7fc7f8bd7bb128658a
Byron Xu <byronxu@microsoft.com>
2023-01-06 12:56:57 -0500

    set vcpkg host triplet in build script

--- .scripts/build-cmake.cmd
+++ .scripts/build-cmake.cmd
@@ -8,7 +8,7 @@ SETLOCAL
 CALL %~dp0init-cmake.cmd
 PUSHD %rlRoot%
 
-cmake -S . -B build -A "x64" -DCMAKE_TOOLCHAIN_FILE="%VcpkgCmake%" -DVCPKG_TARGET_TRIPLET=x64-windows-v141 -DVCPKG_MANIFEST_MODE=Off
+cmake -S . -B build -A "x64" -DCMAKE_TOOLCHAIN_FILE="%VcpkgCmake%" -DVCPKG_TARGET_TRIPLET=x64-windows-v141 -DVCPKG_HOST_TRIPLET=x64-windows-v141 -DVCPKG_MANIFEST_MODE=Off
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build build --config Release
 if %errorlevel% neq 0 exit /b %errorlevel%
