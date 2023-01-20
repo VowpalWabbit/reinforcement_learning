7703abb421560419eecb394e727db39cde8a6a31
Jack Gerrits <jackgerrits@users.noreply.github.com>
2023-01-20 16:08:01 -0500

    Add ctr to cb and add an option to refresh after every n examples (#543)
    * Add ctr to cb and add an option to refresh after every n examples
    
    * formatting

--- examples/rl_sim_cpp/main.cc
+++ examples/rl_sim_cpp/main.cc
@@ -38,7 +38,10 @@ po::variables_map process_cmd_line(const int argc, char** argv)
       "random_seed", po::value<uint64_t>()->default_value(rand()), "Random seed. Default is random")("delay",
       po::value<int64_t>()->default_value(2000),
       "Delay between events in ms")("quiet", po::bool_switch(), "Suppress logs")(
-      "random_ids", po::value<bool>()->default_value(true), "Use randomly generated Event IDs. Default is true");
+      "random_ids", po::value<bool>()->default_value(true), "Use randomly generated Event IDs. Default is true")(
+      "refresh_model_period", po::value<uint64_t>()->default_value(0),
+      "Call refresh model after every N examples. 0 turns off explicit model refresh and relies on background refresh. "
+      "Must disable background refresh in client.json with key 'model.backgroundrefresh'");
 
   po::variables_map vm;
   store(parse_command_line(argc, argv, desc), vm);
