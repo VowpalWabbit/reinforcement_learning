0f330c9a7bbeed0d95d0d196063ed34ac36771af
Griffin Bassman <griffinbassman@gmail.com>
2023-10-03 19:41:00 -0400

    gc distortion stats

--- bindings/cs/rl.net.cli/Statistics.cs
+++ bindings/cs/rl.net.cli/Statistics.cs
@@ -7,16 +7,14 @@ namespace Rl.Net.Cli
     public class Statistics
     {
         public int Messages { get; private set; }
-
         public double Bytes { get; private set; }
-
         public Stopwatch Timer { get; } = Stopwatch.StartNew();
-
         public long ElapsedMicroseconds { get; private set; }
-
         public List<long> ElapsedMicrosecondsPerExample { get; } = new List<long>();
+        private int lastGCCount;
+        public List<bool> IsGCDistorted { get; } = new List<bool>();
 
-        public static Statistics operator + (Statistics s1, Statistics s2)
+        public static Statistics operator +(Statistics s1, Statistics s2)
         {
             return new Statistics()
             {
@@ -30,8 +28,15 @@ namespace Rl.Net.Cli
         {
             Bytes += byteCount;
             Messages++;
+
             long currentElapsedMicroseconds = Timer.ElapsedTicks * 1000000L / Stopwatch.Frequency;
             ElapsedMicrosecondsPerExample.Add(currentElapsedMicroseconds - ElapsedMicroseconds);
+
+            // Check if a GC occurred since the last update
+            int currentGCCount = GC.CollectionCount(0);
+            IsGCDistorted.Add(currentGCCount > lastGCCount);
+            lastGCCount = currentGCCount;
+
             ElapsedMicroseconds = currentElapsedMicroseconds;
         }
 
@@ -49,20 +54,36 @@ namespace Rl.Net.Cli
             Console.WriteLine($"Msg/s: {msgsPerSecond}");
             Console.WriteLine($"Latency: {elapsedSeconds / Messages * 1_000_000} μs");
 
-            // Print average latency of the top 1% most microsecond examples
-            int top1PercentCount = (int)(0.01 * ElapsedMicrosecondsPerExample.Count);
-            if (top1PercentCount > 0)
+            // Compute average latency of the top 1% with GC distortions
+            List<long> allLatencies = new List<long>(ElapsedMicrosecondsPerExample);
+            allLatencies.Sort((a, b) => b.CompareTo(a));
+            int top1PercentCount = (int)(0.01 * allLatencies.Count);
+            long totalTop1PercentLatency = 0;
+            for (int i = 0; i < top1PercentCount; i++)
             {
-                List<long> sortedLatencies = new List<long>(ElapsedMicrosecondsPerExample);
-                sortedLatencies.Sort((a, b) => b.CompareTo(a));  // sort in descending order
-                long totalTop1PercentLatency = 0;
-                for (int i = 0; i < top1PercentCount; i++)
-                {
-                    totalTop1PercentLatency += sortedLatencies[i];
-                }
-                double averageTop1PercentLatency = (double)totalTop1PercentLatency / top1PercentCount;
-                Console.WriteLine($"Average latency of top 1% examples: {averageTop1PercentLatency} μs");
+                totalTop1PercentLatency += allLatencies[i];
             }
+            double averageTop1PercentLatency = (double)totalTop1PercentLatency / top1PercentCount;
+            Console.WriteLine($"Average latency of top 1% examples (with GC distortions): {averageTop1PercentLatency} μs");
+
+            // Compute average latency of the top 1% without GC distortions
+            List<long> filteredLatencies = new List<long>();
+            for (int i = 0; i < ElapsedMicrosecondsPerExample.Count; i++)
+            {
+                if (!IsGCDistorted[i])
+                {
+                    filteredLatencies.Add(ElapsedMicrosecondsPerExample[i]);
+                }
+            }
+            filteredLatencies.Sort((a, b) => b.CompareTo(a));
+            top1PercentCount = (int)(0.01 * filteredLatencies.Count);
+            totalTop1PercentLatency = 0;
+            for (int i = 0; i < top1PercentCount; i++)
+            {
+                totalTop1PercentLatency += filteredLatencies[i];
+            }
+            averageTop1PercentLatency = (double)totalTop1PercentLatency / top1PercentCount;
+            Console.WriteLine($"Average latency of top 1% examples (without GC distortions): {averageTop1PercentLatency} μs");
         }
     }
 }
