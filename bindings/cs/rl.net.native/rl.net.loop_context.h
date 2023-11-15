98a4b65e55d482402dc0c21f9477d7593b10ce29
Griffin Bassman <griffinbassman@gmail.com>
2023-11-15 17:01:00 -0500

    feat: C# bindings for non-cb loop types (#598)
    * feat: CCB Loop Type
    
    * feat: CA Loop Type
    
    * feat: Slates Loop Type
    
    * lint
    
    * add loop type tests
    
    * slates dummy test
    
    * typo

--- bindings/cs/rl.net.native/rl.net.loop_context.h
+++ bindings/cs/rl.net.native/rl.net.loop_context.h
@@ -12,7 +12,7 @@ const char* const BINDING_TRACE_LOGGER = "BINDING_TRACE_LOGGER";
 typedef void (*trace_logger_callback_t)(int log_level, const char* msg);
 }  // namespace rl_net_native
 
-typedef struct base_loop_context
+typedef struct loop_context
 {
   // callback funtion to user when there is background error.
   rl_net_native::background_error_callback_t background_error_callback;
@@ -20,4 +20,4 @@ typedef struct base_loop_context
   rl_net_native::trace_logger_callback_t trace_logger_callback;
   // A trace log factory instance holder of one loop instance for binding calls.
   reinforcement_learning::trace_logger_factory_t* trace_logger_factory;
-} base_loop_context_t;
+} loop_context_t;
