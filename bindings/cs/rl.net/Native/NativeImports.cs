14303e81d94c43853481607d562944e0ee02d435
James Longo <v-jameslongo@microsoft.com>
2024-08-09 16:02:30 -0400

    added set(CMAKE_DEBUG_POSTFIX "") to ensure VS builds have the same lib debug suffixes as the Ninja builds. update the rlnetnative import constant (removing the d suffix)

--- bindings/cs/rl.net/Native/NativeImports.cs
+++ bindings/cs/rl.net/Native/NativeImports.cs
@@ -1,7 +1,9 @@
 namespace Rl.Net.Native {
    internal static class NativeImports {
+      // NOTE: RLNETNATIVE for debug and release are the same,
+      //       but this is a placeholder for future changes.
       #if DEBUG
-      internal const string RLNETNATIVE = "rlnetnatived";
+      internal const string RLNETNATIVE = "rlnetnative";
       #else
       internal const string RLNETNATIVE = "rlnetnative";
       #endif
